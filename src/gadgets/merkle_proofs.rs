e8eb658f8ec046dd840bce6e45aa54a598c1a165
Daniel Lubarov <daniel@lubarov.com>
2021-04-06 19:11:21 -0700

    Candidate API for Merkle proof data
    Does this make sense? I think other libraries tend to include the leaf's index (either as an integer, or a series of bits indicating left/right turns) as part of a "proof". In FRI, the leaf indices are chosen by the verifier, so I thought that approach might be sort of redundant. Let me know what you think though.

---
+++ src/gadgets/merkle_proofs.rs
@@ -0,0 +1,24 @@
+use crate::circuit_builder::CircuitBuilder;
+use crate::field::field::Field;
+use crate::target::Target;
+
+pub struct MerkleProof<F: Field> {
+    /// The Merkle digest of each sibling subtree, staying from the bottommost layer.
+    pub siblings: Vec<F>,
+}
+
+pub struct MerkleProofTarget {
+    /// The Merkle digest of each sibling subtree, staying from the bottommost layer.
+    pub siblings: Vec<Target>,
+}
+
+impl<F: Field> CircuitBuilder<F> {
+    pub(crate) fn verify_merkle_proof(
+        &mut self,
+        leaf_index: Target,
+        leaf_data: Vec<Target>,
+        proof: MerkleProofTarget,
+    ) {
+        todo!()
+    }
+}
