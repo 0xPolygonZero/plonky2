0cd73399408fd84c7d2bbad1e96a8dc2cb2acd09
Daniel Lubarov <daniel@lubarov.com>
2021-04-07 09:10:06 -0700

    Fixes based on PR feedback

--- src/gadgets/merkle_proofs.rs
+++ src/gadgets/merkle_proofs.rs
@@ -1,30 +1,37 @@
 use crate::circuit_builder::CircuitBuilder;
 use crate::field::field::Field;
+use crate::proof::{Hash, HashTarget};
 use crate::target::Target;
 
 pub struct MerkleProof<F: Field> {
     /// The Merkle digest of each sibling subtree, staying from the bottommost layer.
-    pub siblings: Vec<F>,
+    pub siblings: Vec<Hash<F>>,
 }
 
 pub struct MerkleProofTarget {
     /// The Merkle digest of each sibling subtree, staying from the bottommost layer.
-    pub siblings: Vec<Target>,
+    pub siblings: Vec<HashTarget>,
 }
 
+/// Verifies that the given leaf data is present at the given index in the Merkle tree with the
+/// given root.
 pub(crate) fn verify_merkle_proof<F: Field>(
-    leaf_index: usize,
     leaf_data: Vec<F>,
+    leaf_index: usize,
+    merkle_root: Hash<F>,
     proof: MerkleProof<F>,
 ) {
     todo!()
 }
 
 impl<F: Field> CircuitBuilder<F> {
+    /// Verifies that the given leaf data is present at the given index in the Merkle tree with the
+    /// given root.
     pub(crate) fn verify_merkle_proof(
         &mut self,
-        leaf_index: Target,
         leaf_data: Vec<Target>,
+        leaf_index: Target,
+        merkle_root: HashTarget,
         proof: MerkleProofTarget,
     ) {
         todo!()
