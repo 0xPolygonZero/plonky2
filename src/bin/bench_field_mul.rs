f807db388b64531e497740c646eb582f44f3d38a
Daniel Lubarov <daniel@lubarov.com>
2021-04-09 10:26:21 -0700

    Add bench_field_mul_interleaved benchmark

--- src/bin/bench_field_mul.rs
+++ src/bin/bench_field_mul.rs
@@ -1,3 +1,5 @@
+//! Performs a single exponentiation.
+
 use std::time::Instant;
 
 use plonky2::field::crandall_field::CrandallField;
@@ -5,17 +7,19 @@ use plonky2::field::field::Field;
 
 type F = CrandallField;
 
+const EXPONENT: usize = 1000000000;
+
 fn main() {
-    let m = F::from_canonical_u64(12345678901234567890);
-    let mut x = F::ONE;
+    let base = F::rand();
+    let mut state = F::ONE;
+
     let start = Instant::now();
-    let num_muls = 2000000000;
-    for _ in 0..num_muls {
-        x *= m;
+    for _ in 0..EXPONENT {
+        state *= base;
     }
     let duration = start.elapsed();
 
-    println!("result {:?}", x);
-    println!("took {:?}", duration);
-    println!("avg {:?}ns", duration.as_secs_f64() * 1e9 / (num_muls as f64));
+    println!("Result: {:?}", state);
+    println!("Average field mul: {:?}ns",
+             duration.as_secs_f64() * 1e9 / EXPONENT as f64);
 }
