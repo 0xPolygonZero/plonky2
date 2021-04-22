13519e66abc48dea9e96c1f919c2fcaaeae7ab52
wborgeaud <williamborgeaud@gmail.com>
2021-04-22 22:33:29 +0200

    Merkle subtree proofs - WIP

--- src/merkle_proofs.rs
+++ src/merkle_proofs.rs
@@ -1,10 +1,11 @@
 use crate::circuit_builder::CircuitBuilder;
 use crate::field::field::Field;
 use crate::gates::gmimc::GMiMCGate;
-use crate::hash::GMIMC_ROUNDS;
 use crate::hash::{compress, hash_or_noop};
+use crate::hash::{merkle_root_inner, GMIMC_ROUNDS};
 use crate::proof::{Hash, HashTarget};
 use crate::target::Target;
+use crate::util::reverse_index_bits_in_place;
 use crate::wire::Wire;
 use anyhow::{ensure, Result};
 
@@ -34,6 +35,37 @@ pub(crate) fn verify_merkle_proof<F: Field>(
         leaf_index
     };
     let mut current_digest = hash_or_noop(leaf_data);
+    for (i, &sibling_digest) in proof.siblings.iter().enumerate() {
+        let bit = (index >> i & 1) == 1;
+        current_digest = if bit {
+            compress(sibling_digest, current_digest)
+        } else {
+            compress(current_digest, sibling_digest)
+        }
+    }
+    ensure!(current_digest == merkle_root, "Invalid Merkle proof.");
+
+    Ok(())
+}
+
+/// Verifies that the given subtree is present at the given index in the Merkle tree with the
+/// given root.
+pub(crate) fn verify_merkle_proof_subtree<F: Field>(
+    mut subtree_leaves_data: Vec<Vec<F>>,
+    subtree_index: usize,
+    merkle_root: Hash<F>,
+    proof: &MerkleProof<F>,
+    reverse_bits: bool,
+) -> Result<()> {
+    let index = if reverse_bits {
+        // reverse_index_bits_in_place(&mut subtree_leaves_data);
+        crate::util::reverse_bits(subtree_index, proof.siblings.len())
+    } else {
+        subtree_index
+    };
+    dbg!(&subtree_leaves_data);
+    let mut current_digest = merkle_root_inner(subtree_leaves_data);
+    dbg!(current_digest);
     for (i, &sibling_digest) in proof.siblings.iter().enumerate() {
         let bit = (index >> i & 1) == 1;
         current_digest = if bit {
