3fe57cdd313ac615b3ad99a3b748c97f77784735
wborgeaud <williamborgeaud@gmail.com>
2021-04-22 22:33:29 +0200

    Merkle subtree proofs - WIP

--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -76,24 +76,65 @@ impl<F: Field> MerkleTree<F> {
                 .collect(),
         }
     }
+
+    /// Create a Merkle proof for an entire subtree.
+    /// Example:
+    /// ```
+    ///         G
+    ///        / \
+    ///       /   \
+    ///      /     \
+    ///     E       F
+    ///    / \     / \
+    ///   A   B   C   D
+    /// ```
+    /// `self.prove_subtree(0, 1)` gives a Merkle proof for the subtree E->(A,B), i.e., the
+    /// path (F,).
+    pub fn prove_subtree(&self, subtree_index: usize, subtree_height: usize) -> MerkleProof<F> {
+        let index = if self.reverse_bits {
+            reverse_bits(
+                subtree_index,
+                log2_strict(self.leaves.len()) - subtree_height,
+            )
+        } else {
+            subtree_index
+        };
+        MerkleProof {
+            siblings: self
+                .layers
+                .iter()
+                .skip(subtree_height)
+                .scan(index, |acc, layer| {
+                    let index = *acc ^ 1;
+                    *acc >>= 1;
+                    Some(layer[index])
+                })
+                .collect(),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::field::crandall_field::CrandallField;
-    use crate::merkle_proofs::verify_merkle_proof;
+    use crate::merkle_proofs::{verify_merkle_proof, verify_merkle_proof_subtree};
     use crate::polynomial::division::divide_by_z_h;
     use anyhow::Result;
+
+    fn random_data<F: Field>(n: usize, k: usize) -> Vec<Vec<F>> {
+        (0..n)
+            .map(|_| (0..k).map(|_| F::rand()).collect())
+            .collect()
+    }
 
     #[test]
     fn test_merkle_trees() -> Result<()> {
         type F = CrandallField;
 
-        let n = 1 << 10;
-        let leaves: Vec<Vec<F>> = (0..n)
-            .map(|_| (0..10).map(|_| F::rand()).collect())
-            .collect();
+        let log_n = 3;
+        let n = 1 << log_n;
+        let leaves = random_data::<F>(n, 7);
 
         let tree = MerkleTree::new(leaves.clone(), false);
         for i in 0..n {
@@ -101,10 +142,52 @@ mod tests {
             verify_merkle_proof(tree.leaves[i].clone(), i, tree.root, &proof, false)?;
         }
 
+        for height in 0..=log_n {
+            for i in 0..(n >> height) {
+                let subtree_proof = tree.prove_subtree(i, height);
+                verify_merkle_proof_subtree(
+                    tree.leaves[i << height..(i + 1) << height].to_vec(),
+                    i,
+                    tree.root,
+                    &subtree_proof,
+                    false,
+                )?;
+            }
+        }
+
         let tree_reversed_bits = MerkleTree::new(leaves.clone(), true);
         for i in 0..n {
             let proof = tree_reversed_bits.prove(i);
             verify_merkle_proof(leaves[i].clone(), i, tree_reversed_bits.root, &proof, true)?;
+        }
+
+        let (height, i) = (1, 0);
+        dbg!(height, i);
+        let subtree_proof = tree_reversed_bits.prove_subtree(i, height);
+        dbg!(&tree_reversed_bits, &subtree_proof);
+        verify_merkle_proof_subtree(
+            (i << height..(i + 1) << height)
+                .map(|j| tree_reversed_bits.leaves[j].to_vec())
+                .collect(),
+            i,
+            tree_reversed_bits.root,
+            &subtree_proof,
+            true,
+        )?;
+        for height in 1..=log_n {
+            for i in 0..(n >> height) {
+                dbg!(height, i);
+                let subtree_proof = tree_reversed_bits.prove_subtree(i, height);
+                verify_merkle_proof_subtree(
+                    (i << height..(i + 1) << height)
+                        .map(|j| tree_reversed_bits.leaves[j].to_vec())
+                        .collect(),
+                    i,
+                    tree_reversed_bits.root,
+                    &subtree_proof,
+                    true,
+                )?;
+            }
         }
 
         Ok(())
