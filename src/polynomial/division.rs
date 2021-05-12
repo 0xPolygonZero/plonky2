aa7d9d86a55e86068cd193cb847077563d90268d
Daniel Lubarov <daniel@lubarov.com>
2021-05-12 11:20:47 -0700

    Finish merging in old_polynomial

--- src/polynomial/division.rs
+++ src/polynomial/division.rs
@@ -3,73 +3,140 @@ use crate::field::field::Field;
 use crate::polynomial::polynomial::PolynomialCoeffs;
 use crate::util::log2_strict;
 
-/// Takes a polynomial `a` in coefficient form, and divides it by `Z_H = X^n - 1`.
-///
-/// This assumes `Z_H | a`, otherwise result is meaningless.
-pub(crate) fn divide_by_z_h<F: Field>(mut a: PolynomialCoeffs<F>, n: usize) -> PolynomialCoeffs<F> {
-    // TODO: Is this special case needed?
-    if a.coeffs.iter().all(|p| *p == F::ZERO) {
-        return a;
+impl<F: Field> PolynomialCoeffs<F> {
+    /// Polynomial division.
+    /// Returns `(q, r)`, the quotient and remainder of the polynomial division of `a` by `b`.
+    pub(crate) fn div_rem(&self, b: &Self) -> (Self, Self) {
+        let (a_degree_plug_1, b_degree_plus_1) = (self.degree_plus_one(), b.degree_plus_one());
+        if self.is_zero() {
+            (Self::zero(1), Self::empty())
+        } else if b.is_zero() {
+            panic!("Division by zero polynomial");
+        } else if a_degree_plug_1 < b_degree_plus_1 {
+            (Self::zero(1), self.clone())
+        } else if b_degree_plus_1 == 1 {
+            (self * b.coeffs[0].inverse(), Self::empty())
+        } else {
+            let rev_b = b.rev();
+            let rev_b_inv = rev_b.inv_mod_xn(a_degree_plug_1 - b_degree_plus_1 + 1);
+            let rhs: Self = self.rev().coeffs[..=a_degree_plug_1 - b_degree_plus_1]
+                .to_vec()
+                .into();
+            let rev_q: Self = (&rev_b_inv * &rhs).coeffs[..=a_degree_plug_1 - b_degree_plus_1]
+                .to_vec()
+                .into();
+            let mut q = rev_q.rev();
+            let mut qb = &q * b;
+            let mut r = self - &qb;
+            q.trim();
+            r.trim();
+            (q, r)
+        }
     }
 
-    let g = F::MULTIPLICATIVE_GROUP_GENERATOR;
-    let mut g_pow = F::ONE;
-    // Multiply the i-th coefficient of `a` by `g^i`. Then `new_a(w^j) = old_a(g.w^j)`.
-    a.coeffs.iter_mut().for_each(|x| {
-        *x *= g_pow;
-        g_pow *= g;
-    });
+    /// Polynomial long division.
+    /// Returns `(q, r)`, the quotient and remainder of the polynomial division of `a` by `b`.
+    /// Generally slower that the equivalent function `Polynomial::polynomial_division`.
+    pub fn div_rem_long_division(&self, b: &Self) -> (Self, Self) {
+        let b = b.trimmed();
 
-    let root = F::primitive_root_of_unity(log2_strict(a.len()));
-    // Equals to the evaluation of `a` on `{g.w^i}`.
-    let mut a_eval = fft(a);
-    // Compute the denominators `1/(g^n.w^(n*i) - 1)` using batch inversion.
-    let denominator_g = g.exp_usize(n);
-    let root_n = root.exp_usize(n);
-    let mut root_pow = F::ONE;
-    let denominators = (0..a_eval.len())
-        .map(|i| {
-            if i != 0 {
-                root_pow = root_pow * root_n;
+        let (a_degree_plus_1, b_degree_plus_1) = (self.degree_plus_one(), b.degree_plus_one());
+        if self.is_zero() {
+            (Self::zero(1), Self::empty())
+        } else if b.is_zero() {
+            panic!("Division by zero polynomial");
+        } else if a_degree_plus_1 < b_degree_plus_1 {
+            (Self::zero(1), self.clone())
+        } else {
+            // Now we know that self.degree() >= divisor.degree();
+            let mut quotient = Self::zero(a_degree_plus_1 - b_degree_plus_1 + 1);
+            let mut remainder = self.clone();
+            // Can unwrap here because we know self is not zero.
+            let divisor_leading_inv = b.lead().inverse();
+            while !remainder.is_zero() && remainder.degree_plus_one() >= b_degree_plus_1 {
+                let cur_q_coeff = remainder.lead() * divisor_leading_inv;
+                let cur_q_degree = remainder.degree_plus_one() - b_degree_plus_1;
+                quotient.coeffs[cur_q_degree] = cur_q_coeff;
+
+                for (i, &div_coeff) in b.coeffs.iter().enumerate() {
+                    remainder.coeffs[cur_q_degree + i] =
+                        remainder.coeffs[cur_q_degree + i] - (cur_q_coeff * div_coeff);
+                }
+                remainder.trim();
             }
-            denominator_g * root_pow - F::ONE
-        })
-        .collect::<Vec<_>>();
-    let denominators_inv = F::batch_multiplicative_inverse(&denominators);
-    // Divide every element of `a_eval` by the corresponding denominator.
-    // Then, `a_eval` is the evaluation of `a/Z_H` on `{g.w^i}`.
-    a_eval
-        .values
-        .iter_mut()
-        .zip(denominators_inv.iter())
-        .for_each(|(x, &d)| {
-            *x *= d;
+            (quotient, remainder)
+        }
+    }
+
+    /// Takes a polynomial `a` in coefficient form, and divides it by `Z_H = X^n - 1`.
+    ///
+    /// This assumes `Z_H | a`, otherwise result is meaningless.
+    pub(crate) fn divide_by_z_h(&self, n: usize) -> PolynomialCoeffs<F> {
+        let mut a = self.clone();
+
+        // TODO: Is this special case needed?
+        if a.coeffs.iter().all(|p| *p == F::ZERO) {
+            return a;
+        }
+
+        let g = F::MULTIPLICATIVE_GROUP_GENERATOR;
+        let mut g_pow = F::ONE;
+        // Multiply the i-th coefficient of `a` by `g^i`. Then `new_a(w^j) = old_a(g.w^j)`.
+        a.coeffs.iter_mut().for_each(|x| {
+            *x *= g_pow;
+            g_pow *= g;
         });
-    // `p` is the interpolating polynomial of `a_eval` on `{w^i}`.
-    let mut p = ifft(a_eval);
-    // We need to scale it by `g^(-i)` to get the interpolating polynomial of `a_eval` on `{g.w^i}`,
-    // a.k.a `a/Z_H`.
-    let g_inv = g.inverse();
-    let mut g_inv_pow = F::ONE;
-    p.coeffs.iter_mut().for_each(|x| {
-        *x *= g_inv_pow;
-        g_inv_pow *= g_inv;
-    });
-    p
+
+        let root = F::primitive_root_of_unity(log2_strict(a.len()));
+        // Equals to the evaluation of `a` on `{g.w^i}`.
+        let mut a_eval = fft(a);
+        // Compute the denominators `1/(g^n.w^(n*i) - 1)` using batch inversion.
+        let denominator_g = g.exp_usize(n);
+        let root_n = root.exp_usize(n);
+        let mut root_pow = F::ONE;
+        let denominators = (0..a_eval.len())
+            .map(|i| {
+                if i != 0 {
+                    root_pow = root_pow * root_n;
+                }
+                denominator_g * root_pow - F::ONE
+            })
+            .collect::<Vec<_>>();
+        let denominators_inv = F::batch_multiplicative_inverse(&denominators);
+        // Divide every element of `a_eval` by the corresponding denominator.
+        // Then, `a_eval` is the evaluation of `a/Z_H` on `{g.w^i}`.
+        a_eval
+            .values
+            .iter_mut()
+            .zip(denominators_inv.iter())
+            .for_each(|(x, &d)| {
+                *x *= d;
+            });
+        // `p` is the interpolating polynomial of `a_eval` on `{w^i}`.
+        let mut p = ifft(a_eval);
+        // We need to scale it by `g^(-i)` to get the interpolating polynomial of `a_eval` on `{g.w^i}`,
+        // a.k.a `a/Z_H`.
+        let g_inv = g.inverse();
+        let mut g_inv_pow = F::ONE;
+        p.coeffs.iter_mut().for_each(|x| {
+            *x *= g_inv_pow;
+            g_inv_pow *= g_inv;
+        });
+        p
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::field::crandall_field::CrandallField;
     use crate::field::field::Field;
-    use crate::polynomial::division::divide_by_z_h;
     use crate::polynomial::polynomial::PolynomialCoeffs;
 
     #[test]
     fn zero_div_z_h() {
         type F = CrandallField;
         let zero = PolynomialCoeffs::<F>::zero(16);
-        let quotient = divide_by_z_h(zero.clone(), 4);
+        let quotient = zero.divide_by_z_h(4);
         assert_eq!(quotient, zero);
     }
 
@@ -91,7 +158,7 @@ mod tests {
         let a = PolynomialCoeffs::new(vec![-six, -five, -four, -three, six, five, four, three]);
         let q = PolynomialCoeffs::new(vec![six, five, four, three, zero, zero, zero, zero]);
 
-        let computed_q = divide_by_z_h(a, 4);
+        let computed_q = a.divide_by_z_h(4);
         assert_eq!(computed_q, q);
     }
 }
