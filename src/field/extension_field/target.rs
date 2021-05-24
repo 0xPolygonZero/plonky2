4f6e9fb2e031d4147e24e85da5cf9b35032e9ce8
wborgeaud <williamborgeaud@gmail.com>
2021-05-24 17:31:55 +0200

    Recursive evaluation for interpolation gate.

--- src/field/extension_field/target.rs
+++ src/field/extension_field/target.rs
@@ -4,9 +4,22 @@ use crate::field::field::Field;
 use crate::target::Target;
 
 #[derive(Copy, Clone, Debug)]
-pub struct ExtensionTarget<const D: usize>([Target; D]);
+pub struct ExtensionTarget<const D: usize>(pub [Target; D]);
+
+impl<const D: usize> ExtensionTarget<D> {
+    pub fn to_target_array(&self) -> [Target; D] {
+        self.0
+    }
+}
 
 impl<F: Field> CircuitBuilder<F> {
+    pub fn zero_ext<const D: usize>(&mut self) -> ExtensionTarget<D>
+    where
+        F: Extendable<D>,
+    {
+        ExtensionTarget([self.zero(); D])
+    }
+
     pub fn add_extension<const D: usize>(
         &mut self,
         mut a: ExtensionTarget<D>,
@@ -17,6 +30,20 @@ impl<F: Field> CircuitBuilder<F> {
     {
         for i in 0..D {
             a.0[i] = self.add(a.0[i], b.0[i]);
+        }
+        a
+    }
+
+    pub fn sub_extension<const D: usize>(
+        &mut self,
+        mut a: ExtensionTarget<D>,
+        b: ExtensionTarget<D>,
+    ) -> ExtensionTarget<D>
+    where
+        F: Extendable<D>,
+    {
+        for i in 0..D {
+            a.0[i] = self.sub(a.0[i], b.0[i]);
         }
         a
     }
@@ -42,5 +69,20 @@ impl<F: Field> CircuitBuilder<F> {
             }
         }
         ExtensionTarget(res)
+    }
+
+    /// Returns a*b where `b` is in the extension field and `a` is in the base field.
+    pub fn scalar_mul<const D: usize>(
+        &mut self,
+        a: Target,
+        mut b: ExtensionTarget<D>,
+    ) -> ExtensionTarget<D>
+    where
+        F: Extendable<D>,
+    {
+        for i in 0..D {
+            b.0[i] = self.mul(a, b.0[i]);
+        }
+        b
     }
 }
