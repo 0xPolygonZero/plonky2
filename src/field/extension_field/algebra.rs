f9652114356b32b072b0035ace73c06abbbbb147
wborgeaud <williamborgeaud@gmail.com>
2021-06-08 19:36:30 +0200

    Use Daniel's `fmt`

--- src/field/extension_field/algebra.rs
+++ src/field/extension_field/algebra.rs
@@ -34,11 +34,11 @@ impl<F: OEF<D>, const D: usize> From<F> for ExtensionAlgebra<F, D> {
 
 impl<F: OEF<D>, const D: usize> Display for ExtensionAlgebra<F, D> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({}) + ", self.0[0])?;
-        for i in 1..D - 1 {
-            write!(f, "({})*b^{} + ", self.0[i], i)?;
+        write!(f, "({})", self.0[0])?;
+        for i in 1..D {
+            write!(f, " + ({})*b^{}", self.0[i], i)?;
         }
-        write!(f, "({})*b^{}", self.0[D - 1], D - 1)
+        Ok(())
     }
 }
 
