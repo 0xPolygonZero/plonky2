3d318e56dd1a698f0785c72293bc276ac2964195
Jakub Nabaglo <j@nab.gl>
2021-08-24 10:57:14 -0700

    Separate throughput/latency benchmarks

--- benches/field_arithmetic.rs
+++ benches/field_arithmetic.rs
@@ -1,3 +1,5 @@
+#![feature(destructuring_assignment)]
+
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use plonky2::field::crandall_field::CrandallField;
 use plonky2::field::extension_field::quartic::QuarticCrandallField;
@@ -5,39 +7,53 @@ use plonky2::field::field_types::Field;
 use tynm::type_name;
 
 pub(crate) fn bench_field<F: Field>(c: &mut Criterion) {
-    c.bench_function(&format!("mul<{}>", type_name::<F>()), |b| {
+    c.bench_function(&format!("mul-throughput<{}>", type_name::<F>()), |b| {
         b.iter_batched(
-            || (F::rand(), F::rand()),
-            |(x, y)| {
-                let yy = y * y;
-                let xx = x * x;
-                let xy = x * y;
-
-                let xx_x = xx * x;
-                let yy_y = yy * y;
-                let xy_x = xy * x;
-                let xy_y = xy * y;
-
-                (xx_x * xy_x) * (yy_y * xy_y)
+            || (F::rand(), F::rand(), F::rand(), F::rand()),
+            |(mut x, mut y, mut z, mut w)| {
+                for _ in 0..25 {
+                    (x, y, z, w) = (x * y, y * z, z * w, w * x);
+                }
+                (x, y, z, w)
             },
             BatchSize::SmallInput,
         )
     });
 
-    c.bench_function(&format!("add<{}>", type_name::<F>()), |b| {
+    c.bench_function(&format!("mul-latency<{}>", type_name::<F>()), |b| {
         b.iter_batched(
-            || (F::rand(), F::rand()),
-            |(x, y)| {
-                let yy = y + y;
-                let xx = x + x;
-                let xy = x + y;
+            || F::rand(),
+            |(mut x)| {
+                for _ in 0..100 {
+                    x = x * x;
+                }
+                x
+            },
+            BatchSize::SmallInput,
+        )
+    });
 
-                let xx_x = xx + x;
-                let yy_y = yy + y;
-                let xy_x = xy + x;
-                let xy_y = xy + y;
+    c.bench_function(&format!("add-throughput<{}>", type_name::<F>()), |b| {
+        b.iter_batched(
+            || (F::rand(), F::rand(), F::rand(), F::rand()),
+            |(mut x, mut y, mut z, mut w)| {
+                for _ in 0..25 {
+                    (x, y, z, w) = (x + y, y + z, z + w, w + x);
+                }
+                (x, y, z, w)
+            },
+            BatchSize::SmallInput,
+        )
+    });
 
-                (xx_x + xy_x) + (yy_y + xy_y)
+    c.bench_function(&format!("add-latency<{}>", type_name::<F>()), |b| {
+        b.iter_batched(
+            || F::rand(),
+            |mut x| {
+                for _ in 0..100 {
+                    x = x + x;
+                }
+                x
             },
             BatchSize::SmallInput,
         )
