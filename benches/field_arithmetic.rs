d80b71e3e341ecbf14f92f30eaabbe047497ec76
Jakub Nabaglo <j@nab.gl>
2021-08-24 10:00:30 -0700

    Field arithmetic benchmark improvements

--- benches/field_arithmetic.rs
+++ benches/field_arithmetic.rs
@@ -8,7 +8,37 @@ pub(crate) fn bench_field<F: Field>(c: &mut Criterion) {
     c.bench_function(&format!("mul<{}>", type_name::<F>()), |b| {
         b.iter_batched(
             || (F::rand(), F::rand()),
-            |(x, y)| x * y,
+            |(x, y)| {
+                let yy = y * y;
+                let xx = x * x;
+                let xy = x * y;
+
+                let xx_x = xx * x;
+                let yy_y = yy * y;
+                let xy_x = xy * x;
+                let xy_y = xy * y;
+
+                (xx_x * xy_x) * (yy_y * xy_y)
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function(&format!("add<{}>", type_name::<F>()), |b| {
+        b.iter_batched(
+            || (F::rand(), F::rand()),
+            |(x, y)| {
+                let yy = y + y;
+                let xx = x + x;
+                let xy = x + y;
+
+                let xx_x = xx + x;
+                let yy_y = yy + y;
+                let xy_x = xy + x;
+                let xy_y = xy + y;
+
+                (xx_x + xy_x) + (yy_y + xy_y)
+            },
             BatchSize::SmallInput,
         )
     });
