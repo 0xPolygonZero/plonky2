e50d79a347bb7a9a816080e5f063d0c2c49e51c6
Jakub Nabaglo <j@nab.gl>
2021-09-08 16:04:31 -0700

    Tweaks to CrandallField::product

--- src/field/crandall_field.rs
+++ src/field/crandall_field.rs
@@ -431,8 +431,9 @@ impl MulAssign for CrandallField {
 }
 
 impl Product for CrandallField {
+    #[inline]
     fn product<I: Iterator<Item = Self>>(iter: I) -> Self {
-        iter.fold(Self::ONE, |acc, x| acc * x)
+        iter.reduce(|acc, x| acc * x).unwrap_or(Self::ONE)
     }
 }
 
