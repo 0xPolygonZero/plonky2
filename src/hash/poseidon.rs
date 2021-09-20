f28ec6e221da88d833cffdc6cac948889ee5dd92
Jakub Nabaglo <j@nab.gl>
2021-09-20 10:04:51 -0700

    Inlining

--- src/hash/poseidon.rs
+++ src/hash/poseidon.rs
@@ -24,14 +24,14 @@ pub(crate) const N_PARTIAL_ROUNDS: usize = 22;
 const N_ROUNDS: usize = N_FULL_ROUNDS_TOTAL + N_PARTIAL_ROUNDS;
 const MAX_WIDTH: usize = 12; // we only have width 8 and 12, and 12 is bigger. :)
 
-#[inline]
+#[inline(always)]
 fn add_u160_u128((x_lo, x_hi): (u128, u32), y: u128) -> (u128, u32) {
     let (res_lo, over) = x_lo.overflowing_add(y);
     let res_hi = x_hi + (over as u32);
     (res_lo, res_hi)
 }
 
-#[inline]
+#[inline(always)]
 fn reduce_u160<F: PrimeField>((n_lo, n_hi): (u128, u32)) -> F {
     let n_lo_hi = (n_lo >> 64) as u64;
     let n_lo_lo = n_lo as u64;
