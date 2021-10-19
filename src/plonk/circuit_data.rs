dad8ac541c4e4f8843393cf1253195f7da26da32
Daniel Lubarov <daniel@lubarov.com>
2021-10-19 12:38:11 -0700

    Fix

--- src/plonk/circuit_data.rs
+++ src/plonk/circuit_data.rs
@@ -38,20 +38,7 @@ pub struct CircuitConfig {
 
 impl Default for CircuitConfig {
     fn default() -> Self {
-        CircuitConfig {
-            num_wires: 4,
-            num_routed_wires: 4,
-            security_bits: 128,
-            rate_bits: 3,
-            num_challenges: 3,
-            zero_knowledge: true,
-            cap_height: 1,
-            fri_config: FriConfig {
-                proof_of_work_bits: 1,
-                reduction_strategy: FriReductionStrategy::ConstantArityBits(3, 5),
-                num_query_rounds: 1,
-            },
-        }
+        CircuitConfig::standard_recursion_config()
     }
 }
 
@@ -83,7 +70,7 @@ impl CircuitConfig {
         Self {
             num_wires: 143,
             num_routed_wires: 64,
-            security_bits: 128,
+            security_bits: 4,
             rate_bits: 3,
             num_challenges: 3,
             zero_knowledge: false,
