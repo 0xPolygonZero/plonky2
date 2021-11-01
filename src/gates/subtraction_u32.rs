6705d81fbdba21ef9745115865af0cb412894bbe
Nicholas Ward <npward@berkeley.edu>
2021-11-01 11:18:26 -0700

    nit

--- src/gates/subtraction_u32.rs
+++ src/gates/subtraction_u32.rs
@@ -211,7 +211,7 @@ impl<F: RichField + Extendable<D>, const D: usize> Gate<F, D> for U32Subtraction
                 );
                 g
             })
-            .collect::<Vec<_>>()
+            .collect()
     }
 
     fn num_wires(&self) -> usize {
