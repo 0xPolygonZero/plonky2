0a32bbf7b057f3ee99f60825d21f2cd6ee53cc27
Jakub Nabaglo <j@nab.gl>
2021-11-02 11:09:22 -0700

    Rename file to reflect need for fma

--- src/hash/arch/x86_64/mod.rs
+++ src/hash/arch/x86_64/mod.rs
@@ -1,5 +1,5 @@
 // Requires:
 // - AVX2
 // - BMI2 (for MULX and SHRX)
-#[cfg(all(target_feature = "avx2", target_feature = "bmi2"))]
-pub(crate) mod poseidon_goldilocks_avx2_bmi2;
+#[cfg(all(target_feature = "avx2", target_feature = "bmi2", target_feature = "fma"))]
+pub(crate) mod poseidon_goldilocks_avx2_bmi2_fma;
--- src/hash/poseidon_goldilocks.rs
+++ src/hash/poseidon_goldilocks.rs
@@ -350,11 +350,11 @@ impl Poseidon<12> for GoldilocksField {
          0x2c3887c29246a985, 0x863ca0992eae09b0, 0xb8dee12bf8e622dc, ],
     ];
 
-    #[cfg(all(target_arch="x86_64", target_feature="avx2", target_feature="bmi2"))]
+    #[cfg(all(target_arch="x86_64", target_feature="avx2", target_feature="bmi2", target_feature="fma"))]
     #[inline]
     fn poseidon(input: [Self; 12]) -> [Self; 12] {
         unsafe {
-            crate::hash::arch::x86_64::poseidon_goldilocks_avx2_bmi2::poseidon(&input)
+            crate::hash::arch::x86_64::poseidon_goldilocks_avx2_bmi2_fma::poseidon(&input)
         }
     }
 
