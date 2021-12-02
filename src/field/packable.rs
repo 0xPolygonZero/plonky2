6df251e14412295b2463462f3f9bf5c220105713
Jakub Nabaglo <jakub@mirprotocol.org>
2021-12-02 00:01:24 -0800

    Remove `Singleton` type and make every `Field` a `PackedField` (#379)
    * Remove `Singleton` type and make every `Field` a `PackedField`
    
    * Minor: Clippy

--- src/field/packable.rs
+++ src/field/packable.rs
@@ -1,15 +1,15 @@
 use crate::field::field_types::Field;
-use crate::field::packed_field::{PackedField, Singleton};
+use crate::field::packed_field::PackedField;
 
 /// Points us to the default packing for a particular field. There may me multiple choices of
-/// PackedField for a particular Field (e.g. Singleton works for all fields), but this is the
+/// PackedField for a particular Field (e.g. every Field is also a PackedField), but this is the
 /// recommended one. The recommended packing varies by target_arch and target_feature.
 pub trait Packable: Field {
     type PackedType: PackedField<FieldType = Self>;
 }
 
 impl<F: Field> Packable for F {
-    default type PackedType = Singleton<Self>;
+    default type PackedType = Self;
 }
 
 #[cfg(target_feature = "avx2")]
