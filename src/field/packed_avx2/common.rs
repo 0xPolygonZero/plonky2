df984206a0a7ab8a7436bf7a09bf5bf5cab76af2
Jakub Nabaglo <jakub@mirprotocol.org>
2021-12-03 10:58:16 -0800

    Rebase fixes

--- src/field/packed_avx2/common.rs
+++ src/field/packed_avx2/common.rs
@@ -3,7 +3,7 @@ use core::arch::x86_64::*;
 use crate::field::field_types::PrimeField;
 
 pub trait ReducibleAvx2: PrimeField {
-    unsafe fn reduce128s_s(x_s: (__m256i, __m256i)) -> __m256i;
+    unsafe fn reduce128(x: (__m256i, __m256i)) -> __m256i;
 }
 
 const SIGN_BIT: u64 = 1 << 63;
