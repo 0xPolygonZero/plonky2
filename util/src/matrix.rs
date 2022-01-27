8f722dcada571b27af05d365817f7fc0f42fd814
Jakub Nabaglo <jakub@mirprotocol.org>
2022-01-27 12:13:49 -0800

    Allocate with a vec + simplify iterators

--- util/src/matrix.rs
+++ util/src/matrix.rs
@@ -1,10 +1,24 @@
-use std::alloc;
 use std::iter::{DoubleEndedIterator, ExactSizeIterator, FusedIterator, Iterator};
 use std::marker::PhantomData;
 use std::mem::{ManuallyDrop, MaybeUninit};
 use std::ops::{Index, IndexMut};
 use std::ptr;
 use std::slice;
+
+fn size_checked(height: usize, width: usize) -> usize {
+    height.checked_mul(width).expect("memory overflow")
+}
+
+fn uninit_vec<T>(len: usize) -> Vec<MaybeUninit<T>> {
+    let mut res: Vec<MaybeUninit<T>> = Vec::with_capacity(len);
+    unsafe {
+        // SAFETY: `with_capacity` guarantees that `res.capacity() == len`, so we're not violating
+        // memory rules. `MaybeUninit` doesn't need initialization, so the values are already valid.
+        debug_assert_eq!(res.capacity(), len);
+        res.set_len(len);
+    }
+    res
+}
 
 pub struct Matrix<T> {
     data: *mut T,
@@ -14,7 +28,7 @@ pub struct Matrix<T> {
 }
 
 impl<T> Matrix<T> {
-    unsafe fn new_raw(data: *mut T, height: usize, width: usize) -> Self {
+    pub unsafe fn from_raw(data: *mut T, height: usize, width: usize) -> Self {
         Matrix {
             data,
             height,
@@ -23,17 +37,26 @@ impl<T> Matrix<T> {
         }
     }
 
+    pub fn from_flat_vec(height: usize, width: usize, v: Vec<T>) -> Self {
+        // NB: We ask for the height _and_ the width to handle edge cases when `v.len() == 0`.
+
+        let size = size_checked(height, width);
+        assert_eq!(size, v.len(), "matrix dimensions do not match vector length");
+
+        // Get the buffer as a box. This is guaranteed to drop excess capacity; this is important
+        // because to correctly deallocate memory we need to know its allocated size.
+        let buf = v.into_boxed_slice();
+
+        unsafe { Self::from_raw(Box::into_raw(buf).cast(), height, width) }
+    }
+
     pub fn new_uninit(height: usize, width: usize) -> Matrix<MaybeUninit<T>> {
-        let size = height
-            .checked_mul(width)
-            .expect("arithmetic overflow when allocating matrix");
-        let layout =
-            alloc::Layout::array::<T>(size).expect("unable to construct `Layout` for matrix");
-        let data = unsafe { alloc::alloc(layout) };
-        if data.is_null() {
-            alloc::handle_alloc_error(layout);
-        }
-        unsafe { Matrix::new_raw(data.cast(), height, width) }
+        let size = size_checked(height, width);
+        let buf_vec = uninit_vec::<T>(size);
+
+        // We don't need `buf_vec.capacity() == size` for safety, it to prevents reallocation.
+        debug_assert_eq!(buf_vec.capacity(), size);
+        Matrix::from_flat_vec(height, width, buf_vec)
     }
 
     pub unsafe fn new_with<F: FnOnce(&mut Matrix<MaybeUninit<T>>)>(
@@ -46,35 +69,24 @@ impl<T> Matrix<T> {
         res.assume_init()
     }
 
-    pub fn from_flat_vec(height: usize, width: usize, v: Vec<T>) -> Self {
-        // NB: We ask for the height _and_ the width to handle edge cases when `v.len() == 0`.
-
-        let size = height
-            .checked_mul(width)
-            .expect("arithmetic overflow when creating matrix");
-        assert_eq!(
-            size,
-            v.len(),
-            "matrix dimensions ({}x{}={}) do not match vector length ({})",
-            height,
-            width,
-            size,
-            v.len()
-        );
-
-        // Get the buffer as a box. This is guaranteed to drop excess capacity; this is important
-        // because to correctly deallocate memory we need to know its allocated size.
-        let buf = v.into_boxed_slice();
-
-        unsafe { Self::new_raw(Box::into_raw(buf).cast(), height, width) }
-    }
-
     pub const fn height(&self) -> usize {
         self.height
     }
 
     pub const fn width(&self) -> usize {
         self.width
+    }
+
+    pub const fn size(&self) -> usize {
+        self.height * self.width
+    }
+
+    pub fn as_flat(&self) -> &[T] {
+        unsafe { slice::from_raw_parts(self.data, self.size()) }
+    }
+
+    pub fn as_flat_mut(&mut self) -> &mut [T] {
+        unsafe { slice::from_raw_parts_mut(self.data, self.size()) }
     }
 
     pub fn get(&self, index: usize) -> Option<&[T]> {
@@ -94,21 +106,21 @@ impl<T> Matrix<T> {
     }
 
     pub unsafe fn get_unchecked(&self, index: usize) -> &[T] {
-        let row_ptr = self.data.add(index * self.width);
-        slice::from_raw_parts(row_ptr, self.width)
+        self.as_flat().get_unchecked(index * self.width..).get_unchecked(..self.width)
     }
 
     pub unsafe fn get_unchecked_mut(&mut self, index: usize) -> &mut [T] {
-        let row_ptr = self.data.add(index * self.width);
-        slice::from_raw_parts_mut(row_ptr, self.width)
+        let width = self.width;
+        self.as_flat_mut().get_unchecked_mut(index * width..).get_unchecked_mut(..width)
     }
 
-    pub fn iter(&self) -> MatrixIterator<T> {
-        MatrixIterator::new(self.data, self.height, self.width)
+    pub fn iter<'a>(&'a self) -> impl Iterator<Item = &'a [T]> + DoubleEndedIterator + ExactSizeIterator + FusedIterator {
+        self.as_flat().chunks(self.width)
     }
 
-    pub fn iter_mut(&mut self) -> MatrixIteratorMut<T> {
-        MatrixIteratorMut::new(self.data, self.height, self.width)
+    pub fn iter_mut<'a>(&'a mut self) -> impl Iterator<Item = &'a mut [T]> + DoubleEndedIterator + ExactSizeIterator + FusedIterator {
+        let width = self.width;
+        self.as_flat_mut().chunks_mut(width)
     }
 
     pub fn transpose(self) -> Self {
@@ -118,35 +130,25 @@ impl<T> Matrix<T> {
 
 impl<T> Matrix<MaybeUninit<T>> {
     pub unsafe fn assume_init(self) -> Matrix<T> {
+        // Prevent `self` from getting dropped.
+        // Warning: must ensure that we can't crash before making a new Matrix or we will leak! 
         let me = ManuallyDrop::new(self);
-        Matrix::new_raw(me.data.cast(), me.height, me.width)
+        Matrix::from_raw(me.data.cast(), me.height, me.width)
     }
 }
 
 impl<T: Clone> Clone for Matrix<T> {
     fn clone(&self) -> Self {
-        todo!();
-    }
-
-    fn clone_from(&mut self, _source: &Self) {
-        todo!();
+        Self::from_flat_vec(self.height, self.width, self.as_flat().to_vec())
     }
 }
 
 impl<T> Drop for Matrix<T> {
     fn drop(&mut self) {
-        let size = self
-            .height
-            .checked_mul(self.width)
-            .expect("arithmetic overflow when dropping matrix");
+        let sized_data = ptr::slice_from_raw_parts_mut(self.data, self.size());
         unsafe {
-            // Recursively drop
-            ptr::drop_in_place(ptr::slice_from_raw_parts_mut(self.data, size));
-        }
-        let layout =
-            alloc::Layout::array::<T>(size).expect("unable to construct `Layout` for matrix");
-        unsafe {
-            alloc::dealloc(self.data.cast(), layout);
+            // Transfer ownership to a `Box`, which will deallocate when dropped.
+            Box::from_raw(sized_data);
         }
     }
 }
@@ -168,83 +170,5 @@ impl<T> IndexMut<usize> for Matrix<T> {
 impl<T, R: IntoIterator<Item = T>> FromIterator<R> for Matrix<T> {
     fn from_iter<I: IntoIterator<Item = R>>(_iter: I) -> Self {
         todo!();
-    }
-}
-
-pub struct MatrixIterator<'a, T> {
-    _data: *const T,
-    height: usize,
-    _width: usize,
-    _phantom: PhantomData<&'a Matrix<T>>,
-}
-
-impl<T> MatrixIterator<'_, T> {
-    pub(self) fn new(_data: *const T, _height: usize, _width: usize) -> Self {
-        todo!();
-    }
-}
-
-impl<'a, T> Iterator for MatrixIterator<'a, T> {
-    type Item = &'a [T];
-
-    fn next(&mut self) -> Option<Self::Item> {
-        todo!();
-    }
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        (self.len(), Some(self.len()))
-    }
-}
-
-impl<T> DoubleEndedIterator for MatrixIterator<'_, T> {
-    fn next_back(&mut self) -> Option<Self::Item> {
-        todo!();
-    }
-}
-
-impl<T> FusedIterator for MatrixIterator<'_, T> {}
-
-impl<T> ExactSizeIterator for MatrixIterator<'_, T> {
-    fn len(&self) -> usize {
-        self.height
-    }
-}
-
-pub struct MatrixIteratorMut<'a, T> {
-    _data: *mut T,
-    height: usize,
-    _width: usize,
-    _phantom: PhantomData<&'a mut Matrix<T>>,
-}
-
-impl<T> MatrixIteratorMut<'_, T> {
-    pub(self) fn new(_data: *const T, _height: usize, _width: usize) -> Self {
-        todo!();
-    }
-}
-
-impl<'a, T> Iterator for MatrixIteratorMut<'a, T> {
-    type Item = &'a mut [T];
-
-    fn next(&mut self) -> Option<Self::Item> {
-        todo!();
-    }
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        (self.len(), Some(self.len()))
-    }
-}
-
-impl<T> DoubleEndedIterator for MatrixIteratorMut<'_, T> {
-    fn next_back(&mut self) -> Option<Self::Item> {
-        todo!();
-    }
-}
-
-impl<T> FusedIterator for MatrixIteratorMut<'_, T> {}
-
-impl<T> ExactSizeIterator for MatrixIteratorMut<'_, T> {
-    fn len(&self) -> usize {
-        self.height
     }
 }
