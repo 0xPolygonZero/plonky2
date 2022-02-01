0f1b9f6ad0958f6243fb36f34a20efedcbdaeb75
Daniel Lubarov <daniel@lubarov.com>
2022-01-31 17:21:58 -0800

    imports

--- plonky2/src/plonk/recursive_verifier.rs
+++ plonky2/src/plonk/recursive_verifier.rs
@@ -208,7 +208,7 @@ mod tests {
     use crate::fri::FriConfig;
     use crate::gates::noop::NoopGate;
     use crate::iop::witness::{PartialWitness, Witness};
-    use crate::plonk::circuit_data::VerifierOnlyCircuitData;
+    use crate::plonk::circuit_data::{CircuitConfig, VerifierOnlyCircuitData};
     use crate::plonk::config::{GenericConfig, KeccakGoldilocksConfig, PoseidonGoldilocksConfig};
     use crate::plonk::proof::{CompressedProofWithPublicInputs, ProofWithPublicInputs};
     use crate::plonk::prover::prove;
