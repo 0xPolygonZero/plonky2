179a0e742ba25a36ebd0aba7fbffcbaecb0562d9
Shalev Keren <shalev.ke@gmail.com>
2022-02-23 21:18:31 +0200

    WIP: added main structure to MT, need to modify eq circuit still

--- zkrouter/src/main.rs
+++ zkrouter/src/main.rs
@@ -5,32 +5,66 @@ use plonky2::hash::merkle_tree::MerkleTree;
 use plonky2::iop::witness::{PartialWitness, Witness};
 use plonky2::plonk::circuit_builder::CircuitBuilder;
 use plonky2::plonk::circuit_data::CircuitConfig;
-use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
+use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig, Hasher};
 use plonky2::plonk::verifier::verify;
 use plonky2::hash::merkle_proofs::MerkleProofTarget;
+use plonky2_field::goldilocks_field::GoldilocksField;
+use plonky2::hash::poseidon::PoseidonHash;
 use std::time::Instant;
 
 
+const D: usize = 2;
+// This is some optimization used mainly for recursive proofs / internally by FRI - irrelevant ro the zkrouter for now
+const CAP_HEIGHT: usize = 1;
+const SECRET_SIZE: usize = 2;
+type C = PoseidonGoldilocksConfig;
+type F = <C as GenericConfig<D>>::F;
 
-fn random_data<F: Field>(n: usize, k: usize) -> Vec<Vec<F>> {
-    (0..n).map(|_| F::rand_vec(k)).collect()
+// This is the private key used for withdrawing the funds after deposit
+#[derive(Clone)]
+struct DepositPrivateKey {
+    secret: [F; SECRET_SIZE],
+    nonce: [F; SECRET_SIZE]
+}
+
+impl DepositPrivateKey {
+    fn commit_to_key(self) -> Vec<F> {
+        // Hardcoded SECRET_SIZE = 2 to save some clones
+        let hash_in = [self.secret[0], self.secret[1], self.nonce[0], self.nonce[1]];
+        PoseidonHash::hash_no_pad(&hash_in).elements.to_vec()
+    }
+
+    fn generate_random_private_key() -> Self {
+        Self {
+            secret: F::rand_arr::<SECRET_SIZE>(),
+            nonce: F::rand_arr::<SECRET_SIZE>()
+        }
+    }
+}
+
+fn random_data(n: usize) -> Vec<DepositPrivateKey> {
+    (0..n).map(|_| DepositPrivateKey::generate_random_private_key()).collect()
+}
+
+fn generate_merkle_tree(tree_size: usize, deposit_priv_key: DepositPrivateKey, index: usize) -> MerkleTree<GoldilocksField, PoseidonHash> {
+    let leaves_pt = random_data(tree_size);
+    let mut leaves: Vec<Vec<GoldilocksField>> = leaves_pt.into_iter().map(|privkey| privkey.commit_to_key()).collect();
+    leaves[index] = deposit_priv_key.commit_to_key();
+    MerkleTree::<F, <C as GenericConfig<D>>::Hasher>::new(leaves, CAP_HEIGHT)
 }
 
 fn main() {
     let mut start = Instant::now();
-    const D: usize = 2;
-    type C = PoseidonGoldilocksConfig;
-    type F = <C as GenericConfig<D>>::F;
+    
     let config = CircuitConfig::standard_recursion_zk_config();
     let mut pw = PartialWitness::new();
 
-    let log_n = 26;
+    let log_n = 20;
     println!("Testing Merkle Tree with 2^{:?} leaves", log_n);
     let n = 1 << log_n;
-    let cap_height = 1;
-    let leaves = random_data::<F>(n, 7);
-    let tree = MerkleTree::<F, <C as GenericConfig<D>>::Hasher>::new(leaves, cap_height);
+    let my_private_key = DepositPrivateKey::generate_random_private_key();
     let i: usize = thread_rng().gen_range(0..n);
+    let tree = generate_merkle_tree(n, my_private_key.clone(), i);
     let proof = tree.prove(i);
 
     let mut duration = start.elapsed();
@@ -47,7 +81,7 @@ fn main() {
         pw.set_hash_target(proof_t.siblings[i], proof.siblings[i]);
     }
 
-    let cap_t = builder.add_virtual_cap(cap_height);
+    let cap_t = builder.add_virtual_cap(CAP_HEIGHT);
     pw.set_cap_target(&cap_t, &tree.cap);
 
     let i_c = builder.constant(F::from_canonical_usize(i));
