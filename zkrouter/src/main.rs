3c25032a42da05cc23ff1ab6d8aa5b5196a44b3b
shalevos <shalevos@shalevos.io>
2022-03-08 15:36:38 +0200

    WORKING: simple PoC with public inputs

--- zkrouter/src/main.rs
+++ zkrouter/src/main.rs
@@ -7,6 +7,7 @@ use plonky2::iop::witness::{PartialWitness, Witness};
 use plonky2::plonk::circuit_builder::CircuitBuilder;
 use plonky2::plonk::circuit_data::CircuitConfig;
 use plonky2::plonk::config::{GenericConfig, Hasher, PoseidonGoldilocksConfig};
+use plonky2::plonk::proof::CompressedProofWithPublicInputs;
 use plonky2::plonk::verifier::verify;
 use plonky2_field::field_types::Field;
 use plonky2_field::goldilocks_field::GoldilocksField;
@@ -14,7 +15,7 @@ use rand::{thread_rng, Rng};
 
 const D: usize = 2;
 // This is some optimization used mainly for recursive proofs / internally by FRI - irrelevant to the zkrouter for now
-const CAP_HEIGHT: usize = 1;
+const CAP_HEIGHT: usize = 0;
 const SECRET_SIZE: usize = 4;
 type C = PoseidonGoldilocksConfig;
 type F = <C as GenericConfig<D>>::F;
@@ -72,14 +73,14 @@ fn main() {
     let mut start = Instant::now();
 
     let config = CircuitConfig::standard_recursion_zk_config();
-    let mut pw = PartialWitness::new();
-
     let log_n = 20;
     println!("Testing Merkle Tree with 2^{:?} leaves", log_n);
     let n = 1 << log_n;
     let my_private_key = DepositPrivateKey::generate_random_private_key();
+    println!("my_private_key - nonce: {:?}", my_private_key.nonce);
     let private_deposit_index: usize = thread_rng().gen_range(0..n);
     let tree = generate_merkle_tree(n, my_private_key.clone(), private_deposit_index);
+    println!("tree cap: {:?}", tree.cap);
     let proof = tree.prove(private_deposit_index);
 
     let mut duration = start.elapsed();
@@ -89,16 +90,20 @@ fn main() {
     start = Instant::now();
     let mut builder = CircuitBuilder::<F, D>::new(config);
 
+    let cap_t = builder.add_virtual_cap(CAP_HEIGHT);
+    // Register the root of the merkle tree as public input
+    builder.register_public_inputs(&cap_t.0[0].elements);
+    
     let proof_t = MerkleProofTarget {
         siblings: builder.add_virtual_hashes(proof.siblings.len()),
     };
-
-    let cap_t = builder.add_virtual_cap(CAP_HEIGHT);
 
     let i_c = builder.constant(F::from_canonical_usize(private_deposit_index));
     let i_bits = builder.split_le(i_c, log_n);
 
     let targets_for_deposit_privkey = builder.add_virtual_targets(SECRET_SIZE * 2);
+    // Register the nonce used as a public input
+    builder.register_public_inputs(&targets_for_deposit_privkey[SECRET_SIZE..]);
     
     let deposit_privkey_hash = builder.hash_n_to_hash_no_pad::<<C as GenericConfig<D>>::Hasher>(targets_for_deposit_privkey.clone());
 
@@ -114,15 +119,17 @@ fn main() {
     let circuit_data = builder.build::<C>();
 
     // Creating the private witness
+    let mut pw = PartialWitness::new();
+
+    // Set the root of the merkle tree (privately)
+    pw.set_cap_target(&cap_t, &tree.cap);
+
     // Set the sibling wires to their values (privately)
     for i in 0..proof.siblings.len() {
         pw.set_hash_target(proof_t.siblings[i], proof.siblings[i]);
     }
 
-    // Set the root of the merkle tree (privately)
-    pw.set_cap_target(&cap_t, &tree.cap);
-
-    // Set the FEs the comprise the data in our proven leaf (privately)
+    // Set the FEs that comprise our deposit private key (privately)
     let privkey_slice = DepositPrivateKey::as_slice(&my_private_key);
     for j in 0..targets_for_deposit_privkey.len() {
         pw.set_target(
@@ -137,15 +144,19 @@ fn main() {
     // Starting to generate the proof
     start = Instant::now();
     let proof = circuit_data.prove(pw).unwrap();
+    println!("public_inputs: {:?}", proof.public_inputs);
     let compressed_proof = proof.to_owned().compress(&circuit_data.common).unwrap();
+    let compressed_proof_bytes = compressed_proof.to_bytes().unwrap();
+    println!("compressed public_inputs: {:?}", compressed_proof.public_inputs);
     println!("Proof size: {:?}", proof.to_bytes().unwrap().len());
     println!(
         "Compressed proof size: {:?}",
-        compressed_proof.to_bytes().unwrap().len()
+        compressed_proof_bytes.len()
     );
     duration = start.elapsed();
     println!("Proof generation: {:?}", duration);
     start = Instant::now();
+    // let unpacked_compressed_proof = CompressedProofWithPublicInputs::from_bytes(compressed_proof_bytes, &circuit_data.common).unwrap();
     let resc = compressed_proof.verify(&circuit_data.verifier_only, &circuit_data.common);
     duration = start.elapsed();
     println!("Verify compressed: {:?}, took: {:?}", resc, duration);
