aadae27907e1e0425d215da09261eb347a96af14
Daniel Lubarov <daniel@lubarov.com>
2022-03-16 11:30:59 -0700

    into_iter

--- starky/src/util.rs
+++ starky/src/util.rs
@@ -7,7 +7,7 @@ use plonky2::util::transpose;
 pub fn trace_rows_to_poly_values<F: Field, const COLUMNS: usize>(
     trace_rows: Vec<[F; COLUMNS]>,
 ) -> Vec<PolynomialValues<F>> {
-    let trace_row_vecs = trace_rows.iter().map(|row| row.to_vec()).collect_vec();
+    let trace_row_vecs = trace_rows.into_iter().map(|row| row.to_vec()).collect_vec();
     let trace_col_vecs: Vec<Vec<F>> = transpose(&trace_row_vecs);
     trace_col_vecs
         .into_iter()
