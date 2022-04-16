912113137f10d769db74790aa6f17c9ca70a16f1
Remco Bloemen <remco@wicked.ventures>
2022-04-16 18:43:34 +0200

    Generic hashing bench

--- plonky2/benches/hashing.rs
+++ plonky2/benches/hashing.rs
@@ -6,48 +6,38 @@ use plonky2::field::goldilocks_field::GoldilocksField;
 use plonky2::field::ops::Rand;
 use plonky2::hash::blake3::Blake3Hash;
 use plonky2::hash::hash_types::{BytesHash, RichField};
-use plonky2::hash::hashing::SPONGE_WIDTH;
+use plonky2::hash::hashing::{PlonkyPermutation, SPONGE_WIDTH};
 use plonky2::hash::keccak::KeccakHash;
 use plonky2::hash::poseidon::{Poseidon, PoseidonHash};
 use plonky2::plonk::config::Hasher;
 use tynm::type_name;
 
-// TODO: We could create a generic benchmark on the [`Hasher`] trait. This works,
-// except that `rand()`, which we need on [`Hasher::Hash`] is not a trait method.
+fn bench_hasher<F: RichField, H: Hasher<F>>(c: &mut Criterion)
+where
+    H::Hash: Rand,
+{
+    let mut group = c.benchmark_group(type_name::<H>());
 
-pub(crate) fn bench_keccak<F: RichField>(c: &mut Criterion) {
-    c.bench_function("keccak256", |b| {
+    group.bench_function("two_to_one", |b| {
         b.iter_batched(
-            || (BytesHash::<32>::rand(), BytesHash::<32>::rand()),
-            |(left, right)| <KeccakHash<32> as Hasher<F>>::two_to_one(left, right),
+            || (H::Hash::rand(), H::Hash::rand()),
+            |(left, right)| H::two_to_one(left, right),
             BatchSize::SmallInput,
         )
     });
+
+    for size in [0_usize, 1, 2, 4, 8, 16, 32, 64, 128, 256] {
+        group.bench_with_input(&format!("hash_no_pad/{}", size), &size, |b, &size| {
+            b.iter_batched(
+                || F::rand_vec(size),
+                |state| H::hash_no_pad(&state),
+                BatchSize::SmallInput,
+            )
+        });
+    }
 }
 
-pub(crate) fn bench_blake3<F: RichField>(c: &mut Criterion) {
-    c.bench_function("blake3", |b| {
-        b.iter_batched(
-            || (BytesHash::<32>::rand(), BytesHash::<32>::rand()),
-            |(left, right)| <Blake3Hash<32> as Hasher<F>>::two_to_one(left, right),
-            BatchSize::SmallInput,
-        )
-    });
-}
-
-pub(crate) fn bench_poseidon<F: RichField>(c: &mut Criterion) {
-    c.bench_function("poseidon", |b| {
-        b.iter_batched(
-            || {
-                (
-                    <PoseidonHash as Hasher<F>>::Hash::rand(),
-                    <PoseidonHash as Hasher<F>>::Hash::rand(),
-                )
-            },
-            |(left, right)| <PoseidonHash as Hasher<F>>::two_to_one(left, right),
-            BatchSize::SmallInput,
-        )
-    });
+fn bench_poseidon<F: RichField>(c: &mut Criterion) {
     c.bench_function(
         &format!("poseidon<{}, {}>", type_name::<F>(), SPONGE_WIDTH),
         |b| {
@@ -61,8 +51,9 @@ pub(crate) fn bench_poseidon<F: RichField>(c: &mut Criterion) {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    bench_keccak::<GoldilocksField>(c);
-    bench_blake3::<GoldilocksField>(c);
+    bench_hasher::<GoldilocksField, PoseidonHash>(c);
+    bench_hasher::<GoldilocksField, KeccakHash<32>>(c);
+    bench_hasher::<GoldilocksField, Blake3Hash<32>>(c);
     bench_poseidon::<GoldilocksField>(c);
 }
 
