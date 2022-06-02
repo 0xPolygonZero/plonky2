5139d808fedf06589b03a6693927b6a8aeda2d74
Jakub Nabaglo <jakub@mirprotocol.org>
2022-06-02 13:35:27 -0700

    Fix Clippy on `main`

--- ecdsa/src/lib.rs
+++ ecdsa/src/lib.rs
@@ -1,4 +1,7 @@
 #![allow(clippy::needless_range_loop)]
+// Below lint is currently broken and produces false positives.
+// TODO: Remove this override when Clippy is patched.
+#![allow(clippy::derive_partial_eq_without_eq)]
 
 pub mod curve;
 pub mod gadgets;
