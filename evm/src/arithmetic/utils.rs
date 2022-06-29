1a5578f33c338b9b45d84fc0de1a1f03715f837a
Hamish Ivey-Law <hamish@ivey-law.name>
2022-06-29 10:56:59 +1000

    Document range-check warning and fix end value calc.

--- evm/src/arithmetic/utils.rs
+++ evm/src/arithmetic/utils.rs
@@ -1,5 +1,8 @@
 use log::error;
 
+/// Emit an error message regarding unchecked range assumptions.
+/// Assumes the values in `cols` are `[cols[0], cols[0] + 1, ...,
+/// cols[0] + cols.len() - 1]`.
 pub(crate) fn _range_check_error<const RC_BITS: u32>(file: &str, line: u32, cols: &[usize]) {
     error!(
         "{}:{}: arithmetic unit skipped {}-bit range-checks on columns {}--{}: not yet implemented",
@@ -7,7 +10,7 @@ pub(crate) fn _range_check_error<const RC_BITS: u32>(file: &str, line: u32, cols
         file,
         RC_BITS,
         cols[0],
-        cols[0] + cols.len()
+        cols[0] + cols.len() - 1
     );
 }
 
