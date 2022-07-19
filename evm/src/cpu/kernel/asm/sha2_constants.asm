0ddf52ef50e37b4b7382cbf4c8e1dfd0008e1d89
Nicholas Ward <npward@berkeley.edu>
2022-07-19 13:37:17 -0700

    constants as macros instead of functions

--- evm/src/cpu/kernel/asm/sha2_constants.asm
+++ evm/src/cpu/kernel/asm/sha2_constants.asm
@@ -1,647 +1,647 @@
-global sha2_const_0:
-    JUMPDEST
+%macro sha2_const_0
     push 1116352408
+%endmacro
 
-global sha2_const_1:
-    JUMPDEST
+%macro sha2_const_1
     push 1899447441
+%endmacro
 
-global sha2_const_2:
-    JUMPDEST
+%macro sha2_const_2
     push 3049323471
+%endmacro
 
-global sha2_const_3:
-    JUMPDEST
+%macro sha2_const_3
     push 3921009573
+%endmacro
 
-global sha2_const_4:
-    JUMPDEST
+%macro sha2_const_4
     push 961987163
+%endmacro
 
-global sha2_const_5:
-    JUMPDEST
+%macro sha2_const_5
     push 1508970993
+%endmacro
 
-global sha2_const_6:
-    JUMPDEST
+%macro sha2_const_6
     push 2453635748
+%endmacro
 
-global sha2_const_7:
-    JUMPDEST
+%macro sha2_const_7
     push 2870763221
+%endmacro
 
-global sha2_const_8:
-    JUMPDEST
+%macro sha2_const_8
     push 3624381080
+%endmacro
 
-global sha2_const_9:
-    JUMPDEST
+%macro sha2_const_9
     push 310598401
+%endmacro
 
-global sha2_const_10:
-    JUMPDEST
+%macro sha2_const_10
     push 607225278
+%endmacro
 
-global sha2_const_11:
-    JUMPDEST
+%macro sha2_const_11
     push 1426881987
+%endmacro
 
-global sha2_const_12:
-    JUMPDEST
+%macro sha2_const_12
     push 1925078388
+%endmacro
 
-global sha2_const_13:
-    JUMPDEST
+%macro sha2_const_13
     push 2162078206
+%endmacro
 
-global sha2_const_14:
-    JUMPDEST
+%macro sha2_const_14
     push 2614888103
+%endmacro
 
-global sha2_const_15:
-    JUMPDEST
+%macro sha2_const_15
     push 3248222580
+%endmacro
 
-global sha2_const_16:
-    JUMPDEST
+%macro sha2_const_16
     push 3835390401
+%endmacro
 
-global sha2_const_17:
-    JUMPDEST
+%macro sha2_const_17
     push 4022224774
+%endmacro
 
-global sha2_const_18:
-    JUMPDEST
+%macro sha2_const_18
     push 264347078
+%endmacro
 
-global sha2_const_19:
-    JUMPDEST
+%macro sha2_const_19
     push 604807628
+%endmacro
 
-global sha2_const_20:
-    JUMPDEST
+%macro sha2_const_20
     push 770255983
+%endmacro
 
-global sha2_const_21:
-    JUMPDEST
+%macro sha2_const_21
     push 1249150122
+%endmacro
 
-global sha2_const_22:
-    JUMPDEST
+%macro sha2_const_22
     push 1555081692
+%endmacro
 
-global sha2_const_23:
-    JUMPDEST
+%macro sha2_const_23
     push 1996064986
+%endmacro
 
-global sha2_const_24:
-    JUMPDEST
+%macro sha2_const_24
     push 2554220882
+%endmacro
 
-global sha2_const_25:
-    JUMPDEST
+%macro sha2_const_25
     push 2821834349
+%endmacro
 
-global sha2_const_26:
-    JUMPDEST
+%macro sha2_const_26
     push 2952996808
+%endmacro
 
-global sha2_const_27:
-    JUMPDEST
+%macro sha2_const_27
     push 3210313671
+%endmacro
 
-global sha2_const_28:
-    JUMPDEST
+%macro sha2_const_28
     push 3336571891
+%endmacro
 
-global sha2_const_29:
-    JUMPDEST
+%macro sha2_const_29
     push 3584528711
+%endmacro
 
-global sha2_const_30:
-    JUMPDEST
+%macro sha2_const_30
     push 113926993
+%endmacro
 
-global sha2_const_31:
-    JUMPDEST
+%macro sha2_const_31
     push 338241895
+%endmacro
 
-global sha2_const_32:
-    JUMPDEST
+%macro sha2_const_32
     push 666307205
+%endmacro
 
-global sha2_const_33:
-    JUMPDEST
+%macro sha2_const_33
     push 773529912
+%endmacro
 
-global sha2_const_34:
-    JUMPDEST
+%macro sha2_const_34
     push 1294757372
+%endmacro
 
-global sha2_const_35:
-    JUMPDEST
+%macro sha2_const_35
     push 1396182291
+%endmacro
 
-global sha2_const_36:
-    JUMPDEST
+%macro sha2_const_36
     push 1695183700
+%endmacro
 
-global sha2_const_37:
-    JUMPDEST
+%macro sha2_const_37
     push 1986661051
+%endmacro
 
-global sha2_const_38:
-    JUMPDEST
+%macro sha2_const_38
     push 2177026350
+%endmacro
 
-global sha2_const_39:
-    JUMPDEST
+%macro sha2_const_39
     push 2456956037
+%endmacro
 
-global sha2_const_40:
-    JUMPDEST
+%macro sha2_const_40
     push 2730485921
+%endmacro
 
-global sha2_const_41:
-    JUMPDEST
+%macro sha2_const_41
     push 2820302411
+%endmacro
 
-global sha2_const_42:
-    JUMPDEST
+%macro sha2_const_42
     push 3259730800
+%endmacro
 
-global sha2_const_43:
-    JUMPDEST
+%macro sha2_const_43
     push 3345764771
+%endmacro
 
-global sha2_const_44:
-    JUMPDEST
+%macro sha2_const_44
     push 3516065817
+%endmacro
 
-global sha2_const_45:
-    JUMPDEST
+%macro sha2_const_45
     push 3600352804
+%endmacro
 
-global sha2_const_46:
-    JUMPDEST
+%macro sha2_const_46
     push 4094571909
+%endmacro
 
-global sha2_const_47:
-    JUMPDEST
+%macro sha2_const_47
     push 275423344
+%endmacro
 
-global sha2_const_48:
-    JUMPDEST
+%macro sha2_const_48
     push 430227734
+%endmacro
 
-global sha2_const_49:
-    JUMPDEST
+%macro sha2_const_49
     push 506948616
+%endmacro
 
-global sha2_const_50:
-    JUMPDEST
+%macro sha2_const_50
     push 659060556
+%endmacro
 
-global sha2_const_51:
-    JUMPDEST
+%macro sha2_const_51
     push 883997877
+%endmacro
 
-global sha2_const_52:
-    JUMPDEST
+%macro sha2_const_52
     push 958139571
+%endmacro
 
-global sha2_const_53:
-    JUMPDEST
+%macro sha2_const_53
     push 1322822218
+%endmacro
 
-global sha2_const_54:
-    JUMPDEST
+%macro sha2_const_54
     push 1537002063
+%endmacro
 
-global sha2_const_55:
-    JUMPDEST
+%macro sha2_const_55
     push 1747873779
+%endmacro
 
-global sha2_const_56:
-    JUMPDEST
+%macro sha2_const_56
     push 1955562222
+%endmacro
 
-global sha2_const_57:
-    JUMPDEST
+%macro sha2_const_57
     push 2024104815
+%endmacro
 
-global sha2_const_58:
-    JUMPDEST
+%macro sha2_const_58
     push 2227730452
+%endmacro
 
-global sha2_const_59:
-    JUMPDEST
+%macro sha2_const_59
     push 2361852424
+%endmacro
 
-global sha2_const_60:
-    JUMPDEST
+%macro sha2_const_60
     push 2428436474
+%endmacro
 
-global sha2_const_61:
-    JUMPDEST
+%macro sha2_const_61
     push 2756734187
+%endmacro
 
-global sha2_const_62:
-    JUMPDEST
+%macro sha2_const_62
     push 3204031479
+%endmacro
 
-global sha2_const_63:
-    JUMPDEST
+%macro sha2_const_63
     push 3329325298
+%endmacro
 
-global sha2_const:
-    JUMPDEST
+%macro sha2_const
     // stack: i
 
-    // case: 0
+    // case 0
     dup1
     %eq(0)
     %jumpi(sha2_const_0)
     swap1
     
-    // case: 1
+    // case 1
     dup1
     %eq(1)
     %jumpi(sha2_const_1)
     swap1
 
-    // case: 2
+    // case 2
     dup1
     %eq(2)
     %jumpi(sha2_const_2)
     swap1
 
-    // case: 3
+    // case 3
     dup1
     %eq(3)
     %jumpi(sha2_const_3)
     swap1
 
-    // case: 4
+    // case 4
     dup1
     %eq(4)
     %jumpi(sha2_const_4)
     swap1
 
-    // case: 5
+    // case 5
     dup1
     %eq(5)
     %jumpi(sha2_const_5)
     swap1
 
-    // case: 6
+    // case 6
     dup1
     %eq(6)
     %jumpi(sha2_const_6)
     swap1
 
-    // case: 7
+    // case 7
     dup1
     %eq(7)
     %jumpi(sha2_const_7)
     swap1
 
-    // case: 8
+    // case 8
     dup1
     %eq(8)
     %jumpi(sha2_const_8)
     swap1
 
-    // case: 9
+    // case 9
     dup1
     %eq(9)
     %jumpi(sha2_const_9)
     swap1
 
-    // case: 10
+    // case 10
     dup1
     %eq(10)
     %jumpi(sha2_const_10)
     swap1
 
-    // case: 11
+    // case 11
     dup1
     %eq(11)
     %jumpi(sha2_const_11)
     swap1
 
-    // case: 12
+    // case 12
     dup1
     %eq(12)
     %jumpi(sha2_const_12)
     swap1
 
-    // case: 13
+    // case 13
     dup1
     %eq(13)
     %jumpi(sha2_const_13)
     swap1
 
-    // case: 14
+    // case 14
     dup1
     %eq(14)
     %jumpi(sha2_const_14)
     swap1
 
-    // case: 15
+    // case 15
     dup1
     %eq(15)
     %jumpi(sha2_const_15)
     swap1
 
-    // case: 16
+    // case 16
     dup1
     %eq(16)
     %jumpi(sha2_const_16)
     swap1
 
-    // case: 17
+    // case 17
     dup1
     %eq(17)
     %jumpi(sha2_const_17)
     swap1
 
-    // case: 18
+    // case 18
     dup1
     %eq(18)
     %jumpi(sha2_const_18)
     swap1
 
-    // case: 19
+    // case 19
     dup1
     %eq(19)
     %jumpi(sha2_const_19)
     swap1
 
-    // case: 20
+    // case 20
     dup1
     %eq(20)
     %jumpi(sha2_const_20)
     swap1
 
-    // case: 21
+    // case 21
     dup1
     %eq(21)
     %jumpi(sha2_const_21)
     swap1
 
-    // case: 22
+    // case 22
     dup1
     %eq(22)
     %jumpi(sha2_const_22)
     swap1
 
-    // case: 23
+    // case 23
     dup1
     %eq(23)
     %jumpi(sha2_const_23)
     swap1
 
-    // case: 24
+    // case 24
     dup1
     %eq(24)
     %jumpi(sha2_const_24)
     swap1
 
-    // case: 25
+    // case 25
     dup1
     %eq(25)
     %jumpi(sha2_const_25)
     swap1
 
-    // case: 26
+    // case 26
     dup1
     %eq(26)
     %jumpi(sha2_const_26)
     swap1
 
-    // case: 27
+    // case 27
     dup1
     %eq(27)
     %jumpi(sha2_const_27)
     swap1
 
-    // case: 28
+    // case 28
     dup1
     %eq(28)
     %jumpi(sha2_const_28)
     swap1
 
-    // case: 29
+    // case 29
     dup1
     %eq(29)
     %jumpi(sha2_const_29)
     swap1
 
-    // case: 30
+    // case 30
     dup1
     %eq(30)
     %jumpi(sha2_const_30)
     swap1
 
-    // case: 31
+    // case 31
     dup1
     %eq(31)
     %jumpi(sha2_const_31)
     swap1
 
-    // case: 32
+    // case 32
     dup1
     %eq(32)
     %jumpi(sha2_const_32)
     swap1
 
-    // case: 33
+    // case 33
     dup1
     %eq(33)
     %jumpi(sha2_const_33)
     swap1
 
-    // case: 34
+    // case 34
     dup1
     %eq(34)
     %jumpi(sha2_const_34)
     swap1
 
-    // case: 35
+    // case 35
     dup1
     %eq(35)
     %jumpi(sha2_const_35)
     swap1
 
-    // case: 36
+    // case 36
     dup1
     %eq(36)
     %jumpi(sha2_const_36)
     swap1
 
-    // case: 37
+    // case 37
     dup1
     %eq(37)
     %jumpi(sha2_const_37)
     swap1
 
-    // case: 38
+    // case 38
     dup1
     %eq(38)
     %jumpi(sha2_const_38)
     swap1
 
-    // case: 39
+    // case 39
     dup1
     %eq(39)
     %jumpi(sha2_const_39)
     swap1
 
-    // case: 40
+    // case 40
     dup1
     %eq(40)
     %jumpi(sha2_const_40)
     swap1
 
-    // case: 41
+    // case 41
     dup1
     %eq(41)
     %jumpi(sha2_const_41)
     swap1
 
-    // case: 42
+    // case 42
     dup1
     %eq(42)
     %jumpi(sha2_const_42)
     swap1
 
-    // case: 43
+    // case 43
     dup1
     %eq(43)
     %jumpi(sha2_const_43)
     swap1
 
-    // case: 44
+    // case 44
     dup1
     %eq(44)
     %jumpi(sha2_const_44)
     swap1
 
-    // case: 45
+    // case 45
     dup1
     %eq(45)
     %jumpi(sha2_const_45)
     swap1
 
-    // case: 46
+    // case 46
     dup1
     %eq(46)
     %jumpi(sha2_const_46)
     swap1
 
-    // case: 47
+    // case 47
     dup1
     %eq(47)
     %jumpi(sha2_const_47)
     swap1
 
-    // case: 48
+    // case 48
     dup1
     %eq(48)
     %jumpi(sha2_const_48)
     swap1
 
-    // case: 49
+    // case 49
     dup1
     %eq(49)
     %jumpi(sha2_const_49)
     swap1
 
-    // case: 50
+    // case 50
     dup1
     %eq(50)
     %jumpi(sha2_const_50)
     swap1
 
-    // case: 51
+    // case 51
     dup1
     %eq(51)
     %jumpi(sha2_const_51)
     swap1
 
-    // case: 52
+    // case 52
     dup1
     %eq(52)
     %jumpi(sha2_const_52)
     swap1
 
-    // case: 53
+    // case 53
     dup1
     %eq(53)
     %jumpi(sha2_const_53)
     swap1
 
-    // case: 54
+    // case 54
     dup1
     %eq(54)
     %jumpi(sha2_const_54)
     swap1
 
-    // case: 55
+    // case 55
     dup1
     %eq(55)
     %jumpi(sha2_const_55)
     swap1
 
-    // case: 56
+    // case 56
     dup1
     %eq(56)
     %jumpi(sha2_const_56)
     swap1
 
-    // case: 57
+    // case 57
     dup1
     %eq(57)
     %jumpi(sha2_const_57)
     swap1
 
-    // case: 58
+    // case 58
     dup1
     %eq(58)
     %jumpi(sha2_const_58)
     swap1
 
-    // case: 59
+    // case 59
     dup1
     %eq(59)
     %jumpi(sha2_const_59)
     swap1
 
-    // case: 60
+    // case 60
     dup1
     %eq(60)
     %jumpi(sha2_const_60)
     swap1
 
-    // case: 61
+    // case 61
     dup1
     %eq(61)
     %jumpi(sha2_const_61)
     swap1
 
-    // case: 62
+    // case 62
     dup1
     %eq(62)
     %jumpi(sha2_const_62)
     swap1
 
-    // case: 63
+    // case 63
     dup1
     %eq(63)
     %jumpi(sha2_const_63)
     swap1
 
-    // stack: i, k[i]
+    // stack i, k[i]
     pop
-    // stack: k[i]
+    // stack k[i]
+%endmacro
