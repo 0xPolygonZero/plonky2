3f08cca1164d422dce8959cc27747e40de118172
Daniel Lubarov <daniel@lubarov.com>
2022-08-01 14:49:28 -0700

    More constants for kernel ASM
    - `GlobalMetadata` - offsets for global kernel variables in memory
    - `ContextMetadata` - offsets for context-specific kernel variables in memory
    - `GAS_CONSTANTS`, based on the yellowpaper
    
    Also move constants to a separate module since `aggregator` was getting long.

---
+++ evm/src/cpu/kernel/global_metadata.rs
@@ -0,0 +1,23 @@
+/// These metadata fields contain global VM state, stored in the `Segment::Metadata` segment of the
+/// kernel's context (which is zero).
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Debug)]
+pub(crate) enum GlobalMetadata {
+    /// The larger context ID that has been used so far in this execution. Tracking this allows us
+    /// give each new context a unique ID, so that its memory will be zero-initialized.
+    LargestContext = 0,
+}
+
+impl GlobalMetadata {
+    pub(crate) const COUNT: usize = 1;
+
+    pub(crate) fn all() -> [Self; Self::COUNT] {
+        [Self::LargestContext]
+    }
+
+    /// The variable name that gets passed into kernel assembly code.
+    pub(crate) fn var_name(&self) -> &'static str {
+        match self {
+            GlobalMetadata::LargestContext => "GLOBAL_METADATA_LARGEST_CONTEXT",
+        }
+    }
+}
