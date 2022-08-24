4378ff0fc2ea72c72ce8f934c147f99c07541515
Nicholas Ward <npward@berkeley.edu>
2022-08-24 08:41:28 -0700

    progress

--- evm/src/cpu/kernel/asm/helper_functions.asm
+++ evm/src/cpu/kernel/asm/helper_functions.asm
@@ -1,4 +1,6 @@
-global swapn
+global swapn:
+    JUMPDEST
+
     // stack: n, ...
     %eq(1)
     %jumpi(case1)
@@ -33,61 +35,81 @@ global swapn
     %eq(16)
     %jumpi(case16)
 case1:
+    JUMPDEST
     swap1
+    %jump(swapn_end)
 case2:
+    JUMPDEST
     swap2
 case3:
+    JUMPDEST
     swap3
 case4:
+    JUMPDEST
     swap4
 case5:
+    JUMPDEST
     swap5
 case6:
+    JUMPDEST
     swap6
 case7:
+    JUMPDEST
     swap7
 case8:
+    JUMPDEST
     swap8
 case9:
+    JUMPDEST
     swap9
 case10:
+    JUMPDEST
     swap10
 case11:
+    JUMPDEST
     swap11
 case12:
+    JUMPDEST
     swap12
 case13:
+    JUMPDEST
     swap13
 case14:
+    JUMPDEST
     swap14
 case15:
+    JUMPDEST
     swap15
 case16:
+    JUMPDEST
     swap16
 swapn_end:
+    JUMPDEST
 
 
 global insertn:
-    // stack: n, val, ...
-    dup
+    JUMPDEST
+
+    // stack: n, val, ... 
+    dup1
     // stack: n, n, val, ...
     swap2
     // stack: val, n, n, ...
     swap1
     // stack: n, val, n, ...
-    %swapn
+    %jump(swapn)
     // stack: [nth], n, ..., val
     swap1
     // stack: n, [nth], ..., val
 swap_back_loop:
-    // stack: k, k, [kth], ..., [k-1st]
-    dup
+    // stack: k, [kth], ..., [k-1st]
+    dup1
     // stack: k, k, [kth], ..., [k-1st]
     swap2
     // stack: [kth], k, k, ..., [k-1st]
     swap1
     // stack: k, [kth], k, ..., [k-1st]
-    %swapn
+    %jump(swapn)
     // stack: [k-1st], k, ..., [k-2nd], [kth]
     swap1
     // stack: k, [k-1st], ..., [k-2nd], [kth]
