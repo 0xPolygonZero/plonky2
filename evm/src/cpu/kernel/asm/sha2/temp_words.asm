83c959d91e44f0355f8150558a82a83ad4fefc37
Nicholas Ward <npward@berkeley.edu>
2022-09-19 10:33:04 -0700

    opcodes to uppercase, and cleanup

--- evm/src/cpu/kernel/asm/sha2/temp_words.asm
+++ evm/src/cpu/kernel/asm/sha2/temp_words.asm
@@ -1,14 +1,14 @@
 %macro sha2_temp_word1
     // stack: e, f, g, h, K[i], W[i]
-    dup1
+    DUP1
     // stack: e, e, f, g, h, K[i], W[i]
     %sha2_bigsigma_1
     // stack: Sigma_1(e), e, f, g, h, K[i], W[i]
-    swap3
+    SWAP3
     // stack: g, e, f, Sigma_1(e), h, K[i], W[i]
-    swap2
+    SWAP2
     // stack: f, e, g, Sigma_1(e), h, K[i], W[i]
-    swap1
+    SWAP1
     // stack: e, f, g, Sigma_1(e), h, K[i], W[i]
     %sha2_choice
     // stack: Ch(e, f, g), Sigma_1(e), h, K[i], W[i]
@@ -21,11 +21,11 @@
 
 %macro sha2_temp_word2
     // stack: a, b, c
-    dup1
+    DUP1
     // stack: a, a, b, c
     %sha2_bigsigma_0
     // stack: Sigma_0(a), a, b, c
-    swap3
+    SWAP3
     // stack: c, a, b, Sigma_0(a)
     %sha2_majority
     // stack: Maj(c, a, b), Sigma_0(a)
--- evm/src/cpu/kernel/asm/sha2/write_length.asm
+++ evm/src/cpu/kernel/asm/sha2/write_length.asm
@@ -1,145 +1,145 @@
 %macro sha2_write_length
     // stack: last_addr, length
-    swap1
+    SWAP1
     // stack: length, last_addr
-    push 1
-    push 8
-    shl
+    PUSH 1
+    PUSH 8
+    SHL
 
     // stack: 1 << 8, length, last_addr
-    dup1
+    DUP1
     // stack: 1 << 8, 1 << 8, length, last_addr
-    dup3
+    DUP3
     // stack: length, 1 << 8, 1 << 8, length, last_addr
-    mod
+    MOD
     // stack: length % (1 << 8), 1 << 8, length, last_addr
-    dup4
+    DUP4
     // stack: last_addr, length % (1 << 8), 1 << 8, length, last_addr
     %mstore_kernel_general
     
     // stack: 1 << 8, length, last_addr
-    dup1
+    DUP1
     // stack: 1 << 8, 1 << 8, length, last_addr
-    dup3
+    DUP3
     // stack: length, 1 << 8, 1 << 8, length, last_addr
-    push 8
-    shr
+    PUSH 8
+    SHR
     // stack: length >> 8, 1 << 8, 1 << 8, length, last_addr
-    mod
+    MOD
     // stack: (length >> 8) % (1 << 8), 1 << 8, length, last_addr
-    dup4
+    DUP4
     // stack: last_addr, (length >> 8) % (1 << 8), 1 << 8, length, last_addr
-    push 1
-    swap1
-    sub
+    PUSH 1
+    SWAP1
+    SUB
     // stack: last_addr - 1, (length >> 8) % (1 << 8), 1 << 8, length, last_addr
     %mstore_kernel_general
 
     // stack: 1 << 8, length, last_addr
-    dup1
+    DUP1
     // stack: 1 << 8, 1 << 8, length, last_addr
-    dup3
+    DUP3
     // stack: length, 1 << 8, 1 << 8, length, last_addr
-    push 16
-    shr
+    PUSH 16
+    SHR
     // stack: length >> 16, 1 << 8, 1 << 8, length, last_addr
-    mod
+    MOD
     // stack: (length >> 16) % (1 << 8), 1 << 8, length, last_addr
-    dup4
+    DUP4
     // stack: last_addr, (length >> 16) % (1 << 8), 1 << 8, length, last_addr
-    push 2
-    swap1
-    sub
+    PUSH 2
+    SWAP1
+    SUB
     // stack: last_addr - 2, (length >> 16) % (1 << 8), 1 << 8, length, last_addr
     %mstore_kernel_general
 
     // stack: 1 << 8, length, last_addr
-    dup1
+    DUP1
     // stack: 1 << 8, 1 << 8, length, last_addr
-    dup3
+    DUP3
     // stack: length, 1 << 8, 1 << 8, length, last_addr
-    push 24
-    shr
+    PUSH 24
+    SHR
     // stack: length >> 24, 1 << 8, 1 << 8, length, last_addr
-    mod
+    MOD
     // stack: (length >> 24) % (1 << 8), 1 << 8, length, last_addr
-    dup4
+    DUP4
     // stack: last_addr, (length >> 24) % (1 << 8), 1 << 8, length, last_addr
-    push 3
-    swap1
-    sub
+    PUSH 3
+    SWAP1
+    SUB
     // stack: last_addr - 3, (length >> 24) % (1 << 8), 1 << 8, length, last_addr
     %mstore_kernel_general
 
     // stack: 1 << 8, length, last_addr
-    dup1
+    DUP1
     // stack: 1 << 8, 1 << 8, length, last_addr
-    dup3
+    DUP3
     // stack: length, 1 << 8, 1 << 8, length, last_addr
-    push 32
-    shr
+    PUSH 32
+    SHR
     // stack: length >> 32, 1 << 8, 1 << 8, length, last_addr
-    mod
+    MOD
     // stack: (length >> 32) % (1 << 8), 1 << 8, length, last_addr
-    dup4
+    DUP4
     // stack: last_addr, (length >> 32) % (1 << 8), 1 << 8, length, last_addr
-    push 4
-    swap1
-    sub
+    PUSH 4
+    SWAP1
+    SUB
     // stack: last_addr - 4, (length >> 32) % (1 << 8), 1 << 8, length, last_addr
     %mstore_kernel_general
 
     // stack: 1 << 8, length, last_addr
-    dup1
+    DUP1
     // stack: 1 << 8, 1 << 8, length, last_addr
-    dup3
+    DUP3
     // stack: length, 1 << 8, 1 << 8, length, last_addr
-    push 40
-    shr
+    PUSH 40
+    SHR
     // stack: length >> 40, 1 << 8, 1 << 8, length, last_addr
-    mod
+    MOD
     // stack: (length >> 40) % (1 << 8), 1 << 8, length, last_addr
-    dup4
+    DUP4
     // stack: last_addr, (length >> 40) % (1 << 8), 1 << 8, length, last_addr
-    push 5
-    swap1
-    sub
+    PUSH 5
+    SWAP1
+    SUB
     // stack: last_addr - 5, (length >> 40) % (1 << 8), 1 << 8, length, last_addr
     %mstore_kernel_general
 
     // stack: 1 << 8, length, last_addr
-    dup1
+    DUP1
     // stack: 1 << 8, 1 << 8, length, last_addr
-    dup3
+    DUP3
     // stack: length, 1 << 8, 1 << 8, length, last_addr
-    push 48
-    shr
+    PUSH 48
+    SHR
     // stack: length >> 48, 1 << 8, 1 << 8, length, last_addr
-    mod
+    MOD
     // stack: (length >> 48) % (1 << 8), 1 << 8, length, last_addr
-    dup4
+    DUP4
     // stack: last_addr, (length >> 48) % (1 << 8), 1 << 8, length, last_addr
-    push 6
-    swap1
-    sub
+    PUSH 6
+    SWAP1
+    SUB
     // stack: last_addr - 6, (length >> 48) % (1 << 8), 1 << 8, length, last_addr
     %mstore_kernel_general
 
     // stack: 1 << 8, length, last_addr
-    dup1
+    DUP1
     // stack: 1 << 8, 1 << 8, length, last_addr
-    dup3
+    DUP3
     // stack: length, 1 << 8, 1 << 8, length, last_addr
-    push 56
-    shr
+    PUSH 56
+    SHR
     // stack: length >> 56, 1 << 8, 1 << 8, length, last_addr
-    mod
+    MOD
     // stack: (length >> 56) % (1 << 8), 1 << 8, length, last_addr
-    dup4
+    DUP4
     // stack: last_addr, (length >> 56) % (1 << 8), 1 << 8, length, last_addr
-    push 7
-    swap1
-    sub
+    PUSH 7
+    SWAP1
+    SUB
     // stack: last_addr - 7, (length >> 56) % (1 << 8), 1 << 8, length, last_addr
     %mstore_kernel_general
     %pop3
