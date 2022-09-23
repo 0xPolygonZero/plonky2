843baf1aa0c2618c7048413c30867f82675549c5
Nicholas Ward <npward@berkeley.edu>
2022-09-23 15:09:56 -0700

    documentation

--- plonky2/examples/fibonacci.rs
+++ plonky2/examples/fibonacci.rs
@@ -5,15 +5,18 @@ use plonky2::plonk::circuit_builder::CircuitBuilder;
 use plonky2::plonk::circuit_data::CircuitConfig;
 use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
 
+/// An example of using Plonky2 to prove a statement of the form
+/// "I know the 100th element of the Fibonacci sequence, starting with constants a and b."
+/// When a == 0 and b == 1, this is proving knowledge of the 100th (standard) Fibonacci number.
 fn main() -> Result<()> {
     const D: usize = 2;
     type C = PoseidonGoldilocksConfig;
     type F = <C as GenericConfig<D>>::F;
 
     let config = CircuitConfig::standard_recursion_config();
-
     let mut builder = CircuitBuilder::<F, D>::new(config);
 
+    // The arithmetic circuit.
     let initial_a = builder.add_virtual_target();
     let initial_b = builder.add_virtual_target();
     let mut prev_target = initial_a;
@@ -23,10 +26,13 @@ fn main() -> Result<()> {
         prev_target = cur_target;
         cur_target = temp;
     }
+
+    // Public inputs are the two initial values (provided below) and the result (which is generated).
     builder.register_public_input(initial_a);
     builder.register_public_input(initial_b);
     builder.register_public_input(cur_target);
 
+    // Provide initial values.
     let mut pw = PartialWitness::new();
     pw.set_target(initial_a, F::ZERO);
     pw.set_target(initial_b, F::ONE);
