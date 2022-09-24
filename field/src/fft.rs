7af8463cb3043cf90e2fe3a12bc5b90d4e2a7fdb
Emanuele Cesena <emanuele.cesena@gmail.com>
2022-09-24 11:34:05 +0200

    Improve twiddle access for FFT - est. 10%+ faster
    Co-authored-by: Nicolas Stalder <nicolas@jumpcrypto.com>
    Co-authored-by: Rahul Maganti <rmaganti@jumpcrypto.com>
    Co-authored-by: Kaveh Aasaraai <kaasaraai@jumptrading.com>
    Co-authored-by: Javier Varela <jvarela@jumptrading.com>
    Co-authored-by: Kevin Bowers <kbowers@jumptrading.com>

--- field/src/fft.rs
+++ field/src/fft.rs
@@ -1,8 +1,6 @@
-use std::cmp::{max, min};
 use std::option::Option;
 
 use plonky2_util::{log2_strict, reverse_index_bits_in_place};
-use unroll::unroll_for_loops;
 
 use crate::packable::Packable;
 use crate::packed::PackedField;
@@ -13,22 +11,19 @@ pub type FftRootTable<F> = Vec<Vec<F>>;
 
 pub fn fft_root_table<F: Field>(n: usize) -> FftRootTable<F> {
     let lg_n = log2_strict(n);
-    // bases[i] = g^2^i, for i = 0, ..., lg_n - 1
-    let mut bases = Vec::with_capacity(lg_n);
-    let mut base = F::primitive_root_of_unity(lg_n);
-    bases.push(base);
-    for _ in 1..lg_n {
-        base = base.square(); // base = g^2^_
-        bases.push(base);
-    }
+    let base = F::primitive_root_of_unity(lg_n);
 
-    let mut root_table = Vec::with_capacity(lg_n);
-    for lg_m in 1..=lg_n {
-        let half_m = 1 << (lg_m - 1);
-        let base = bases[lg_n - lg_m];
-        let root_row = base.powers().take(half_m.max(2)).collect();
-        root_table.push(root_row);
+    let mut root_table = Vec::with_capacity(1);
+    let half_n = 1 << (lg_n - 1);
+    let mut root_row = vec![F::ZERO; half_n];
+    // store roots of unity in "reverse bits" order
+    // faster than calling: reverse_index_bits_in_place(&mut root_row[..])
+    for (i, b) in base.powers().take(half_n).enumerate() {
+        let j = i.reverse_bits() >> (64 - lg_n + 1);
+        root_row[j] = b;
     }
+    root_table.push(root_row);
+
     root_table
 }
 
@@ -45,7 +40,7 @@ fn fft_dispatch<F: Field>(
     };
     let used_root_table = root_table.or(computed_root_table.as_ref()).unwrap();
 
-    fft_classic(input, zero_factor.unwrap_or(0), used_root_table);
+    fft_bowers(input, zero_factor.unwrap_or(0), used_root_table);
 }
 
 #[inline]
@@ -94,11 +89,17 @@ pub fn ifft_with_options<F: Field>(
     PolynomialCoeffs { coeffs: buffer }
 }
 
-/// Generic FFT implementation that works with both scalar and packed inputs.
-#[unroll_for_loops]
-fn fft_classic_simd<P: PackedField>(
+/// FFT implementation that works with both scalar and packed inputs.
+/// Bowers et al., Improved Twiddle Access for Fast Fourier Transforms
+/// https://doi.org/10.1109/TSP.2009.2035984
+/// In short, Bowers et al. rearrange the computation so that
+/// the *twiddle is the same* within the inner-most loop.
+/// Surprisingly, this ends up looking like a decimation in time loop,
+/// but with a decimation in frequency butterfly!
+/// In our experiments this is 10%+ faster than a classic DIT.
+fn fft_bowers_simd<P: PackedField>(
     values: &mut [P::Scalar],
-    r: usize,
+    _r: usize,
     lg_n: usize,
     root_table: &FftRootTable<P::Scalar>,
 ) {
@@ -107,55 +108,34 @@ fn fft_classic_simd<P: PackedField>(
     let packed_n = packed_values.len();
     debug_assert!(packed_n == 1 << (lg_n - lg_packed_width));
 
-    // Want the below for loop to unroll, hence the need for a literal.
-    // This loop will not run when P is a scalar.
-    assert!(lg_packed_width <= 4);
-    for lg_half_m in 0..4 {
-        if (r..min(lg_n, lg_packed_width)).contains(&lg_half_m) {
-            // Intuitively, we split values into m slices: subarr[0], ..., subarr[m - 1]. Each of
-            // those slices is split into two halves: subarr[j].left, subarr[j].right. We do
-            // (subarr[j].left[k], subarr[j].right[k])
-            //   := f(subarr[j].left[k], subarr[j].right[k], omega[k]),
-            // where f(u, v, omega) = (u + omega * v, u - omega * v).
-            let half_m = 1 << lg_half_m;
-
-            // Set omega to root_table[lg_half_m][0..half_m] but repeated.
-            let mut omega = P::default();
-            for (j, omega_j) in omega.as_slice_mut().iter_mut().enumerate() {
-                *omega_j = root_table[lg_half_m][j % half_m];
-            }
-
-            for k in (0..packed_n).step_by(2) {
-                // We have two vectors and want to do math on pairs of adjacent elements (or for
-                // lg_half_m > 0, pairs of adjacent blocks of elements). .interleave does the
-                // appropriate shuffling and is its own inverse.
-                let (u, v) = packed_values[k].interleave(packed_values[k + 1], half_m);
-                let t = omega * v;
-                (packed_values[k], packed_values[k + 1]) = (u + t).interleave(u - t, half_m);
-            }
-        }
-    }
-
-    // We've already done the first lg_packed_width (if they were required) iterations.
-    let s = max(r, lg_packed_width);
-
-    for lg_half_m in s..lg_n {
+    // decimation in time loop
+    for lg_half_m in 0..lg_n {
         let lg_m = lg_half_m + 1;
         let m = 1 << lg_m; // Subarray size (in field elements).
         let packed_m = m >> lg_packed_width; // Subarray size (in vectors).
         let half_packed_m = packed_m / 2;
         debug_assert!(half_packed_m != 0);
 
-        // omega values for this iteration, as slice of vectors
-        let omega_table = P::pack_slice(&root_table[lg_half_m][..]);
-        for k in (0..packed_n).step_by(packed_m) {
+        // k = 0 unrolled: w^0 = 1, save the mul
+        for j in 0..half_packed_m {
+            let u = packed_values[j];
+            let v = packed_values[j + half_packed_m];
+            packed_values[j] = u + v;
+            packed_values[half_packed_m + j] = u - v;
+        }
+
+        let mut omega_idx = 1;
+        for k in (packed_m..packed_n).step_by(packed_m) {
+            // use the same omega for the whole inner loop!
+            let omega = root_table[0][omega_idx];
             for j in 0..half_packed_m {
-                let omega = omega_table[j];
-                let t = omega * packed_values[k + half_packed_m + j];
+                // decimation in frequency butterlfy
                 let u = packed_values[k + j];
-                packed_values[k + j] = u + t;
-                packed_values[k + half_packed_m + j] = u - t;
+                let v = packed_values[k + j + half_packed_m];
+                packed_values[k + j] = u + v;
+                packed_values[k + half_packed_m + j] = (u - v) * omega;
             }
+            omega_idx += 1;
         }
     }
 }
@@ -166,42 +146,27 @@ fn fft_classic_simd<P: PackedField>(
 /// The parameter r signifies that the first 1/2^r of the entries of
 /// input may be non-zero, but the last 1 - 1/2^r entries are
 /// definitely zero.
-pub(crate) fn fft_classic<F: Field>(values: &mut [F], r: usize, root_table: &FftRootTable<F>) {
+pub(crate) fn fft_bowers<F: Field>(values: &mut [F], r: usize, root_table: &FftRootTable<F>) {
     reverse_index_bits_in_place(values);
 
     let n = values.len();
     let lg_n = log2_strict(n);
 
-    if root_table.len() != lg_n {
+    if root_table.len() != 1 {
         panic!(
             "Expected root table of length {}, but it was {}.",
-            lg_n,
+            1,
             root_table.len()
         );
-    }
-
-    // After reverse_index_bits, the only non-zero elements of values
-    // are at indices i*2^r for i = 0..n/2^r.  The loop below copies
-    // the value at i*2^r to the positions [i*2^r + 1, i*2^r + 2, ...,
-    // (i+1)*2^r - 1]; i.e. it replaces the 2^r - 1 zeros following
-    // element i*2^r with the value at i*2^r.  This corresponds to the
-    // first r rounds of the FFT when there are 2^r zeros at the end
-    // of the original input.
-    if r > 0 {
-        // if r == 0 then this loop is a noop.
-        let mask = !((1 << r) - 1);
-        for i in 0..n {
-            values[i] = values[i & mask];
-        }
     }
 
     let lg_packed_width = log2_strict(<F as Packable>::Packing::WIDTH);
     if lg_n <= lg_packed_width {
         // Need the slice to be at least the width of two packed vectors for the vectorized version
         // to work. Do this tiny problem in scalar.
-        fft_classic_simd::<F>(values, r, lg_n, root_table);
+        fft_bowers_simd::<F>(values, r, lg_n, root_table);
     } else {
-        fft_classic_simd::<<F as Packable>::Packing>(values, r, lg_n, root_table);
+        fft_bowers_simd::<<F as Packable>::Packing>(values, r, lg_n, root_table);
     }
 }
 
