44045bd63e5083773a76d2821f813bf40722f30a
Dmitry Vagner <typecat@dmitrys-mbp.lan>
2022-09-29 12:16:13 -0700

    add fp2_add

--- evm/src/cpu/kernel/asm/curve/bn254/field_arithmetic.asm
+++ evm/src/cpu/kernel/asm/curve/bn254/field_arithmetic.asm
@@ -1,44 +1,63 @@
+global fp2_add:
+    // stack: a0, a1, b0, b1
+    PUSH pr
+    SWAP2
+    SWAP3
+    // stack: b0, a0, pr, a1, b1
+    ADDMOD
+    // stack: c0, a1, b1
+    PUSH pr
+    SWAP2
+    // stack: a1, c0, pr, b1
+    SWAP1
+    SWAP3 
+    // stack: b1, a1, pr, c0
+    ADDMOD
+    // stack: c1, c0
+    SWAP1
+    // stack: c0, c1  
+
+
 // [a0, a1]*[b0, b1] = [a0b0 - a1b1, a0b1 + a1b0]
 
-global fp2:
+global fp2_mul:
     // stack:                 a0, a1, b0, b1
-    PUSH pp
-    PUSH pp
-    DUP
-    DUP
+    PUSH pr
+    PUSH pr
+    DUP3
+    DUP6
     MULMOD
-    // stack:       a0b1, pp, a0, a1, b0, b1
-    PUSH pp
-    DUP
-    DUP
+    // stack:       a0b1, pr, a0, a1, b0, b1
+    PUSH pr
+    DUP5
+    DUP8
     MULMOD
-    // stack: a1b0, a0b1, pp, a0, a1, b0, b1
+    // stack: a1b0, a0b1, pr, a0, a1, b0, b1
     ADDMOD
     // stack:             c1, a0, a1, b0, b1
     SWAP4
     // stack:             b1, a0, a1, b0, c1
     SWAP3
     // stack:             b0, a0, a1, b1, c1
-    PUSH pp
+    PUSH pr
     SWAP2
-    // stack:         a0, b0, pp, a1, b1, c1 
+    // stack:         a0, b0, pr, a1, b1, c1 
     MULMOD
     // stack:               a0b0, a1, b1, c1
     SWAP2
     // stack:               b1, a1, a0b0, c1
-    PUSH pp
+    PUSH pr
     SWAP2
-    // stack:           a1, b1, pp, a0b0, c1
+    // stack:           a1, b1, pr, a0b0, c1
     MULMOD
     // stack:                 a1b1, a0b0, c1
-    PUSH pp
+    PUSH pr
     SUB
     // stack:                -a1b1, a0b0, c1
-    PUSH pp
+    PUSH pr
     SWAP2
     ADDMOD
     // stack:                         c0, c1
-
 
 y_mul:
     // stack: a0, a1, a2, a3, a4, a5
@@ -52,30 +71,29 @@ y_mul:
     %mul_9_plus_i
     // stack: c4, c5, a0, a1, a2, a3
 
-
 %macro mul_9_plus_i
     // stack:                b0, b1
-    PUSH pp
-    PUSH pp
-    DUP
+    PUSH pr
+    PUSH pr
+    DUP3
     PUSH 9
-    // stack: 9, b0, pp, pp, b0, b1
+    // stack: 9, b0, pr, pr, b0, b1
     MULMOD
-    // stack:       9b0, pp, b0, b1
+    // stack:       9b0, pr, b0, b1
     DUP3
     ADDMOD
     // stack:      b1 + 9b0, b0, b1
     SWAP2
     // stack:           b1,  b0, c1
-    PUSH pp
+    PUSH pr
     SUB
     // stack:          -b1,  b0, c1
-    PUSH pp
-    PUSH pp
+    PUSH pr
+    PUSH pr
     SWAP3
-    // stack:   b0, pp, -b1, pp, c1   
+    // stack:   b0, pr, -b1, pr, c1   
     PUSH 9
-    // stack:    9, b0, pp, -b1, c1 
+    // stack:    9, b0, pr, -b1, c1 
     MULMOD
     // stack:      9b0,     -b1, c1
     ADDMOD
