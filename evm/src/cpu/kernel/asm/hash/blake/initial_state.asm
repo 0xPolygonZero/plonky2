d3e5feba836ec9ab0569e3bca0f55dd527c7dfea
Nicholas Ward <npward@berkeley.edu>
2022-11-09 10:29:13 -0800

    Blake progress

--- evm/src/cpu/kernel/asm/hash/blake/initial_state.asm
+++ evm/src/cpu/kernel/asm/hash/blake/initial_state.asm
@@ -1,5 +1,4 @@
-global blake_initial_state:
-    // stack: retdest
+%macro blae_initial_state
     %blake_iv(7)
     %blake_iv(6)
     %blake_iv(5)
@@ -7,10 +6,11 @@ global blake_initial_state:
     %blake_iv(3)
     %blake_iv(2)
     %blake_iv(1)
-    // stack: IV_1, IV_2, IV_3, IV_4, IV_5, IV_6, IV_7, retdest
+    // stack: IV_1, IV_2, IV_3, IV_4, IV_5, IV_6, IV_7
     PUSH 0x01010040 // params: key = 00, digest_size = 64 = 0x40
     %blake_iv(0)
     XOR
-    // stack: IV_0 ^ params, IV_1, IV_2, IV_3, IV_4, IV_5, IV_6, IV_7, retdest
+    // stack: IV_0 ^ params, IV_1, IV_2, IV_3, IV_4, IV_5, IV_6, IV_7
     %stack () -> (0, 0)
-    // stack: c_0 = 0, c_1 = 0, h_0, h_1, h_2, h_3, h_4, h_5, h_6, h_7, retdest
+    // stack: c_0 = 0, c_1 = 0, h_0, h_1, h_2, h_3, h_4, h_5, h_6, h_7
+%endmacro
