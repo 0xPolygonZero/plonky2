a38b1fb37c8815069289be4749f477e60cf1ebab
Nicholas Ward <npward@berkeley.edu>
2022-11-10 15:40:44 -0800

    progress

--- evm/src/cpu/kernel/asm/hash/blake/compression.asm
+++ evm/src/cpu/kernel/asm/hash/blake/compression.asm
@@ -1,3 +1,4 @@
+// Load the initial hash value (the IV, but with params XOR'd into the first word).
 %macro blake_initial_hash_value
     %blake_iv_i(7)
     %blake_iv_i(6)
@@ -13,6 +14,7 @@
     // stack: IV_0 ^ params, IV_1, IV_2, IV_3, IV_4, IV_5, IV_6, IV_7
 %endmacro
 
+// Address where the working version of the hash value is stored.
 %macro blake_hash_value_addr
     PUSH 0
     // stack: 0
@@ -23,11 +25,13 @@
     // stack: num_bytes+1
 %endmacro
 
+// Address where the working version of the compression internal state is stored.
 %macro blake_internal_state_addr
     %blake_hash_value_addr
     %add_const(8)
 %endmacro
 
+// Address where the current message block is stored.
 %macro blake_message_addr
     %blake_internal_state_addr
     %add_const(16)
