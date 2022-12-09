a1ea7ff93056c7ac8cdf6c7e80c1e9deb6dec06f
Nicholas Ward <npward@berkeley.edu>
2022-12-09 09:34:42 -0800

    progress

--- evm/src/cpu/kernel/asm/hash/blake/compression.asm
+++ evm/src/cpu/kernel/asm/hash/blake/compression.asm
@@ -1,2 +1,7 @@
+%macro blake_compression_internal_state_addr
+    PUSH 0
+%endmacro
+
 global blake_compression:
-    // stack: 
+    // stack: h_0, ..., h_7, t_0, t_1, f_0, f_1, m_0, ..., m_15
+    
