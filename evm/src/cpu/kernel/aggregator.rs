80b2d70bc1df47fbb0266ceb56dbe76e3a5966e7
Nicholas Ward <npward@berkeley.edu>
2022-12-13 15:13:12 -0800

    test

--- evm/src/cpu/kernel/aggregator.rs
+++ evm/src/cpu/kernel/aggregator.rs
@@ -11,6 +11,7 @@ pub static KERNEL: Lazy<Kernel> = Lazy::new(combined_kernel);
 
 pub(crate) fn combined_kernel() -> Kernel {
     let files = vec![
+        include_str!("asm/bignum.asm"),
         include_str!("asm/core/bootloader.asm"),
         include_str!("asm/core/create.asm"),
         include_str!("asm/core/create_addresses.asm"),
--- evm/src/cpu/kernel/tests/bignum.rs
+++ evm/src/cpu/kernel/tests/bignum.rs
@@ -1,34 +1,53 @@
-use std::str::FromStr;
-
 use anyhow::Result;
 use ethereum_types::U256;
-use rand::{thread_rng, Rng};
-use ripemd::{Digest, Ripemd160};
-use sha2::Sha256;
+use rand::Rng;
 
-use crate::cpu::kernel::aggregator::combined_kernel;
-use crate::cpu::kernel::interpreter::run_with_kernel;
+use crate::cpu::kernel::aggregator::KERNEL;
+use crate::cpu::kernel::interpreter::Interpreter;
 
-fn to_be_limbs(x: U256) -> (usize, Vec<u8>) {
-    let mut len = 0;
+fn to_be_limbs(x: U256) -> Vec<u8> {
+    let mut cur = x;
+
     let mut limbs: Vec<u8> = Vec::new();
-    while x > U256::zero() {
-        len += 1;
-        limbs.push((x % 256).try_into().unwrap());
-        x = x / 256;
+    while cur > U256::zero() {
+        limbs.push((cur % 256).try_into().unwrap());
+        cur = cur / 256;
     }
 
-    (len, limbs)
+    limbs
 }
 
 #[test]
 fn test_add_bignum() -> Result<()> {
     let mut rng = rand::thread_rng();
-    let a: U256 = rng.gen();
-    let b: U256 = rng.gen();
+    let a: U256 = U256(rng.gen::<[u64; 4]>());
+    let b: U256 = U256(rng.gen::<[u64; 4]>());
+    let sum = a + b;
 
-    let (a_len, a_limbs) = to_be_limbs(a);
-    let (b_len, b_limbs) = to_be_limbs(b);
+    let a_limbs = to_be_limbs(a);
+    let b_limbs = to_be_limbs(b);
+
+    let expected_sum = to_be_limbs(sum);
+
+    let a_len = a_limbs.len().into();
+    let b_len = b_limbs.len().into();
+    let a_start_loc = 0.into();
+    let b_start_loc = a_limbs.len().into();
+    let memory: Vec<_> = [&a_limbs[..], &b_limbs[..]].concat().into();
+
+    let retdest = 0xDEADBEEFu32.into();
+    let mut initial_stack: Vec<U256> = vec![a_len, b_len, a_start_loc, b_start_loc, retdest];
+    initial_stack.reverse();
+
+    let add_bignum = KERNEL.global_labels["add_bignum"];
+    let mut interpreter = Interpreter::new_with_kernel(add_bignum, initial_stack);
+    interpreter.set_kernel_general_memory(memory);
+
+    interpreter.run()?;
+
+    let new_memory = interpreter.get_kernel_general_memory();
+    let actual_sum: Vec<u8> = new_memory[..expected_sum.len()].into();
+    assert_eq!(actual_sum, expected_sum);
 
     Ok(())
 }
@@ -37,7 +56,6 @@ fn test_add_bignum() -> Result<()> {
 // fn test_ge_unbounded(x: U256, p: U256) -> Result<()> {
 //     let mut initial_stack = vec![U256::from(message.len())];
 
-    
 //     let bytes: Vec<U256> = message.iter().map(|&x| U256::from(x as u32)).collect();
 //     initial_stack.extend(bytes);
 //     initial_stack.push(U256::from_str("0xdeadbeef").unwrap());
