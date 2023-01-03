03b30b69883d1e1f7910eccf55b3828134a28f5e
Nicholas Ward <npward@berkeley.edu>
2023-01-03 12:26:54 -0800

    updates

--- evm/src/cpu/kernel/asm/bignum/mul.asm
+++ evm/src/cpu/kernel/asm/bignum/mul.asm
@@ -45,20 +45,50 @@ reduce_loop:
     // stack: i, n, i, retdest
     %mload_kernel_general
     // stack: bignum[i], n, i, retdest
+    PUSH 1
+    %shl_const(128)
+    // stack: 2^128, bignum[i], n, i, retdest
+    %stack (mod, val) -> (val, mod, mod, val)
+    // stack: bignum[i], 2^128, 2^128, bignum[i], n, i, retdest
+    MOD
+    // stack: bignum[i] % 2^128, 2^128, bignum[i], n, i, retdest
+    SWAP2
+    // stack: bignum[i], 2^128, bignum[i] % 2^128, n, i, retdest
+    DIV
+    // stack: bignum[i] // 2^128, bignum[i] % 2^128, n, i, retdest
+    DUP4
+    // stack: i, bignum[i] // 2^128, bignum[i] % 2^128, n, i, retdest
+    %increment
+    // stack: i+1, bignum[i] // 2^128, bignum[i] % 2^128, n, i, retdest
+    SWAP1
+    // stack: bignum[i] // 2^128, i+1, bignum[i] % 2^128, n, i, retdest
+    DUP2
+    // stack: i+1, bignum[i] // 2^128, i+1, bignum[i] % 2^128, n, i, retdest
+    %mload_kernel_general
+    // stack: bignum[i+1], bignum[i] // 2^128, i+1, bignum[i] % 2^128, n, i, retdest
+    ADD
+    // stack: bignum[i+1] + bignum[i] // 2^128, i+1, bignum[i] % 2^128, n, i, retdest
+    SWAP1
+    // stack: i+1, bignum[i+1] + bignum[i] // 2^128, bignum[i] % 2^128, n, i, retdest
+    %mstore_kernel_general
+    // stack: bignum[i] % 2^128, n, i, retdest
+    DUP3
+    // stack: i, bignum[i] % 2^128, n, i, retdest
+    %mstore_kernel_general
     // stack: n, i, retdest
     %decrement
     SWAP1
     %increment
     SWAP1
-    // stack: n - 1, i + 1, carry, retdest
+    // stack: n - 1, i + 1, retdest
     DUP1
-    // stack: n - 1, n - 1, i + 1, carry, retdest
+    // stack: n - 1, n - 1, i + 1, retdest
     ISZERO
     %jumpi(reduce_end)
     %jump(reduce_loop)
 reduce_end:
-    // stack: n = 0, i, carry, retdest
-    %stack (vals: 3) -> ()
+    // stack: n = 0, i, retdest
+    %stack (vals: 2) -> ()
     // stack: retdest
     JUMP
 
