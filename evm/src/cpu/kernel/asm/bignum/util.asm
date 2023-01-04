bae5bf0b6799b2ac80426951f57e46595b040033
Nicholas Ward <npward@berkeley.edu>
2023-01-03 16:03:44 -0800

    fix

--- evm/src/cpu/kernel/asm/bignum/util.asm
+++ evm/src/cpu/kernel/asm/bignum/util.asm
@@ -1,6 +1,6 @@
 %macro memcpy_kernel_general
     // stack: dst, src, len
-    %stack (dst, src, len) -> (0, @SEGMENT_KERNEL_GENERAL, $dst, 0, @SEGMENT_KERNEL_GENERAL, $src, $len, %%after)
+    %stack (dst, src, len) -> (0, @SEGMENT_KERNEL_GENERAL, dst, 0, @SEGMENT_KERNEL_GENERAL, src, len, %%after)
     %jump(memcpy)
 %%after:
 %endmacro
