d99cadebc2ab7dedafa23d23d753589635fc924a
Dmitry Vagner <dmitry.vagner@gmail.com>
2023-01-20 16:03:24 +0700

    stack macro

--- evm/src/cpu/kernel/asm/curve/bn254/field_arithmetic/inverse.asm
+++ evm/src/cpu/kernel/asm/curve/bn254/field_arithmetic/inverse.asm
@@ -36,7 +36,7 @@ global inv_fp12:
     // stack:                ptr, inv, retdest
     PUSH 50  PUSH check_inv
     // stack: check_inv, 50, ptr, inv, retdest 
-    SWAP3  SWAP1  SWAP2
+    %stack(check_inv, 50, ptr, inv) -> (ptr, inv, 50, check_inv)
     // stack: ptr, inv, 50, check_inv, retdest 
     %jump(mul_fp12)
 global check_inv:
