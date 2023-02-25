29f0692efc5a611e34591bc061c46377465a5da5
Daniel Lubarov <daniel@lubarov.com>
2023-02-24 23:10:26 -0800

    Fix a few issues found by EVM tests

--- evm/src/cpu/kernel/asm/mpt/storage/storage_read.asm
+++ evm/src/cpu/kernel/asm/mpt/storage/storage_read.asm
@@ -1,30 +1,32 @@
 // Read a word from the current account's storage trie.
 //
-// Pre stack: slot, retdest
+// Pre stack: kexit_info, slot
 // Post stack: value
 
-global storage_read:
-    // stack: slot, retdest
+global sys_sload:
+    // stack: kexit_info, slot
+    SWAP1
+    // stack: slot, kexit_info
     %stack (slot) -> (slot, after_storage_read)
     %slot_to_storage_key
-    // stack: storage_key, after_storage_read, retdest
+    // stack: storage_key, after_storage_read, kexit_info
     PUSH 64 // storage_key has 64 nibbles
     %current_storage_trie
-    // stack: storage_root_ptr, 64, storage_key, after_storage_read, retdest
+    // stack: storage_root_ptr, 64, storage_key, after_storage_read, kexit_info
     %jump(mpt_read)
 
 after_storage_read:
-    // stack: value_ptr, retdest
+    // stack: value_ptr, kexit_info
     DUP1 %jumpi(storage_key_exists)
 
     // Storage key not found. Return default value_ptr = 0,
     // which derefs to 0 since @SEGMENT_TRIE_DATA[0] = 0.
-    %stack (value_ptr, retdest) -> (retdest, 0)
-    JUMP
+    %stack (value_ptr, kexit_info) -> (kexit_info, 0)
+    EXIT_KERNEL
 
 storage_key_exists:
-    // stack: value_ptr, retdest
+    // stack: value_ptr, kexit_info
     %mload_trie_data
-    // stack: value, retdest
+    // stack: value, kexit_info
     SWAP1
-    JUMP
+    EXIT_KERNEL
