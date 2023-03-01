1ee39b51c1ee695e2ca9aa6a041e4c0993b67412
Dmitry Vagner <dmitry.vagner@gmail.com>
2023-02-28 18:57:17 -0800

    fmt

--- evm/src/cpu/kernel/tests/bls381.rs
+++ evm/src/cpu/kernel/tests/bls381.rs
@@ -1,12 +1,13 @@
-use crate::bls381_arithmetic::Fp;
 use rand::Rng;
 
+use crate::bls381_arithmetic::Fp;
+
 #[test]
-fn test_bls_mul() -> Result<(),()> {
+fn test_bls_mul() -> Result<(), ()> {
     let mut rng = rand::thread_rng();
     let f: Fp = rng.gen::<Fp>();
     let g: Fp = rng.gen::<Fp>();
-    let fg = f*g;
+    let fg = f * g;
 
     println!("{:#?}", f);
     println!("{:#?}", g);
