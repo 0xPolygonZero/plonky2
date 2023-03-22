0e8f6a2f681e94edfc6d520b062812659724c6ee
Dmitry Vagner <dmitry.vagner@gmail.com>
2023-03-21 21:17:46 -0700

    test skeleton

--- evm/src/cpu/kernel/asm/curve/bls381/util.asm
+++ evm/src/cpu/kernel/asm/curve/bls381/util.asm
@@ -39,3 +39,15 @@
     %pop4
     // stack:                 z0, z1
 %endmacro
+
+global test_add_fp381:
+    %add_fp381
+    %jump(0xdeadbeef)
+
+global test_mul_fp381:
+    %add_fp381
+    %jump(0xdeadbeef)
+
+global test_sub_fp381:
+    %add_fp381
+    %jump(0xdeadbeef)
