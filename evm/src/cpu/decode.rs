310107f218c9a1829b99bef755fdc93b9f03cb22
Hamy Ratoanina <hamy.ratoanina@toposware.com>
2023-04-07 17:06:57 -0400

    Fix decode constraint

--- evm/src/cpu/decode.rs
+++ evm/src/cpu/decode.rs
@@ -191,7 +191,7 @@ pub fn eval_packed_generic<P: PackedField>(
             .into_iter()
             .zip(bits_from_opcode(oc))
             .rev()
-            .take(block_length + 1)
+            .take(8 - block_length)
             .map(|(row_bit, flag_bit)| match flag_bit {
                 // 1 if the bit does not match, and 0 otherwise
                 false => row_bit,
@@ -264,7 +264,7 @@ pub fn eval_ext_circuit<F: RichField + Extendable<D>, const D: usize>(
             .into_iter()
             .zip(bits_from_opcode(oc))
             .rev()
-            .take(block_length + 1)
+            .take(8 - block_length)
             .fold(builder.zero_extension(), |cumul, (row_bit, flag_bit)| {
                 let to_add = match flag_bit {
                     false => row_bit,
