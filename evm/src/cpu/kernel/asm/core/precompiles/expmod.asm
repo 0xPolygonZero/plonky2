539f28459b24d4256e843484a1c3aca9069b1ccf
wborgeaud <williamborgeaud@gmail.com>
2023-04-13 08:12:27 +0200

    bn_add

--- evm/src/cpu/kernel/asm/core/precompiles/expmod.asm
+++ evm/src/cpu/kernel/asm/core/precompiles/expmod.asm
@@ -1,4 +1,3 @@
 global precompile_expmod:
-    %stack (address, retdest, address, gas, kexit_info, value, args_offset, args_size, ret_offset, ret_size) ->
-        //(args_offset, args_size, ret_offset, ret_size, kexit_info)
-        (args_size, kexit_info, args_offset, args_size, ret_offset, ret_size)
+    // TODO
+    PANIC
