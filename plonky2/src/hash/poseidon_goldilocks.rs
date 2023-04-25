29a8367b8c1dd91156bdfc3ef5fa618e1f770777
Nicholas Ward <npward@berkeley.edu>
2023-04-24 17:11:35 -0700

    fmt

--- plonky2/src/hash/poseidon_goldilocks.rs
+++ plonky2/src/hash/poseidon_goldilocks.rs
@@ -4,9 +4,6 @@
 //! `poseidon_constants.sage` script in the `mir-protocol/hash-constants`
 //! repository.
 
-
-
-
 use crate::field::goldilocks_field::GoldilocksField;
 use crate::hash::poseidon::{Poseidon, N_PARTIAL_ROUNDS};
 
