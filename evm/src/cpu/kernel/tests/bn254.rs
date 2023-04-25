e08c80b6226e80549b73048ad0d97fb986af38d5
wborgeaud <williamborgeaud@gmail.com>
2023-04-25 11:31:23 +0200

    Failing test

--- evm/src/cpu/kernel/tests/bn254.rs
+++ evm/src/cpu/kernel/tests/bn254.rs
@@ -8,6 +8,7 @@ use crate::bn254_pairing::{
 use crate::cpu::kernel::interpreter::{
     run_interpreter_with_memory, Interpreter, InterpreterMemoryInitialization,
 };
+use crate::cpu::kernel::tests::u256ify;
 use crate::extension_tower::{FieldExt, Fp12, Fp2, Fp6, Stack, BN254};
 use crate::memory::segments::Segment::BnPairing;
 
@@ -293,6 +294,21 @@ fn test_bn_pairing() -> Result<()> {
         TWISTED_GENERATOR.y.re.val,
         TWISTED_GENERATOR.y.im.val,
     ];
+    let inputs: Vec<U256> = u256ify(vec![
+        "0x1c76476f4def4bb94541d57ebba1193381ffa7aa76ada664dd31c16024c43f59",
+        "0x3034dd2920f673e204fee2811c678745fc819b55d3e9d294e45c9b03a76aef41",
+        "0x209dd15ebff5d46c4bd888e51a93cf99a7329636c63514396b4a452003a35bf7",
+        "0x4bf11ca01483bfa8b34b43561848d28905960114c8ac04049af4b6315a41678",
+        "0x2bb8324af6cfc93537a2ad1a445cfd0ca2a71acd7ac41fadbf933c2a51be344d",
+        "0x120a2a4cf30c1bf9845f20c6fe39e07ea2cce61f0c9bb048165fe5e4de877550",
+        "0x111e129f1cf1097710d41c4ac70fcdfa5ba2023c6ff1cbeac322de49d1b6df7c",
+        "0x2032c61a830e3c17286de9462bf242fca2883585b93870a73853face6a6bf411",
+        "0x198e9393920d483a7260bfb731fb5d25f1aa493335a9e71297e485b7aef312c2",
+        "0x1800deef121f1e76426a00665e5c4479674322d4f75edadd46debd5cd992f6ed",
+        "0x90689d0585ff075ec9e99ad690c3395bc4b313370b38ef355acdadcd122975b",
+        "0x12c85ea5db8c6deb4aab71808dcb408fe3d1e7690c43d37b4ce6cc0166fa7daa",
+    ])
+    .unwrap();
 
     let setup = InterpreterMemoryInitialization {
         label: "bn254_pairing".to_string(),
@@ -309,6 +325,7 @@ fn test_bn_pairing() -> Result<()> {
     // let output: Vec<U256> = interpreter.extract_kernel_memory(BnPairing, out..out + 12);
     let expected = tate(CURVE_GENERATOR, TWISTED_GENERATOR).on_stack();
     println!("{:?}", expected);
+    dbg!(interpreter.stack()[0]);
     assert_eq!(interpreter.stack()[0], U256::one());
 
     Ok(())
