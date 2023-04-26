e642b8241040be03a834c75ef9d25dfb8a7866e2
Nicholas Ward <npward@berkeley.edu>
2023-04-25 17:26:37 -0700

    move serialization to separate example

--- plonky2/examples/fibonacci_serialization.rs
+++ plonky2/examples/fibonacci_serialization.rs
@@ -1,5 +1,7 @@
 #![allow(clippy::upper_case_acronyms)]
 
+use std::fs;
+
 use anyhow::Result;
 use plonky2::field::types::Field;
 use plonky2::iop::witness::{PartialWitness, WitnessWrite};
@@ -10,6 +12,7 @@ use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
 /// An example of using Plonky2 to prove a statement of the form
 /// "I know the 100th element of the Fibonacci sequence, starting with constants a and b."
 /// When a == 0 and b == 1, this is proving knowledge of the 100th (standard) Fibonacci number.
+/// This example also serializes the circuit data and proof to JSON files.
 fn main() -> Result<()> {
     const D: usize = 2;
     type C = PoseidonGoldilocksConfig;
@@ -39,8 +42,23 @@ fn main() -> Result<()> {
     pw.set_target(initial_a, F::ZERO);
     pw.set_target(initial_b, F::ONE);
 
-    let data = builder.build::<C>();
+    let data = builder.build_without_randomizing::<C>();
+
+    let common_circuit_data_serialized = serde_json::to_string(&data.common).unwrap();
+    fs::write("common_circuit_data.json", common_circuit_data_serialized)
+        .expect("Unable to write file");
+
+    let verifier_only_circuit_data_serialized = serde_json::to_string(&data.verifier_only).unwrap();
+    fs::write(
+        "verifier_only_circuit_data.json",
+        verifier_only_circuit_data_serialized,
+    )
+    .expect("Unable to write file");
+
     let proof = data.prove(pw)?;
+
+    let proof_serialized = serde_json::to_string(&proof).unwrap();
+    fs::write("proof_with_public_inputs.json", proof_serialized).expect("Unable to write file");
 
     println!(
         "100th Fibonacci number mod |F| (starting with {}, {}) is: {}",
