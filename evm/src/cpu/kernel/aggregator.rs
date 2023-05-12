566180f33aa5ed29e374f65d57565afbbf9ec580
wborgeaud <williamborgeaud@gmail.com>
2023-05-12 12:35:51 +0200

    Update aggregator.rs

--- evm/src/cpu/kernel/aggregator.rs
+++ evm/src/cpu/kernel/aggregator.rs
@@ -42,6 +42,7 @@ pub(crate) fn combined_kernel() -> Kernel {
         include_str!("asm/core/util.asm"),
         include_str!("asm/core/access_lists.asm"),
         include_str!("asm/core/selfdestruct_list.asm"),
+        include_str!("asm/core/touched_addresses.asm"),
         include_str!("asm/core/precompiles/main.asm"),
         include_str!("asm/core/precompiles/ecrec.asm"),
         include_str!("asm/core/precompiles/sha256.asm"),
