34e69b3d1ae7b031e2f821c4cb127eeb5ae9c367
Dmitry Vagner <dmitry.vagner@gmail.com>
2023-05-31 12:38:03 -0700

    slicker

--- evm/src/cpu/kernel/asm/curve/bls381/degree_12_mul.asm
+++ evm/src/cpu/kernel/asm/curve/bls381/degree_12_mul.asm
@@ -20,10 +20,10 @@ mul_fp381_12_h0:
     %stack () -> (112, mul_fp381_12_h1)
     // stack:              112, ret, inA, inB, out, jumpdest  { 100: H0 }
     DUP4
-    %add_const(12)
+    %shift_bls_fp6
     // stack:        inB1, 112, ret, inA, inB, out, jumpdest  { 100: H0 }
     DUP4
-    %add_const(12)
+    %shift_bls_fp6
     // stack:  inA1, inB1, 112, ret, inA, inB, out, jumpdest  { 100: H0 }
     %jump(mul_fp381_6)
 mul_fp381_12_h1:
@@ -35,20 +35,20 @@ mul_fp381_12_h0h1:
     // stack:                        inA, inB, out, jumpdest  { 100: H0, 112: H1, 124: H0+H1 }
     %stack (inA) -> (inA, inA, 136, mul_fp381_12_a01)
     // stack:       inA , inA , 136, ret, inB, out, jumpdest  { 100: H0, 112: H1, 124: H0+H1 }
-    %add_const(12)
+    %shift_bls_fp6
     // stack:       inA1, inA0, 136, ret, inB, out, jumpdest  { 100: H0, 112: H1, 124: H0+H1 }
     %jump(add_fp381_6)
 mul_fp381_12_a01:
     // stack:                             inB, out, jumpdest  { 100: H0, 112: H1, 124: H0+H1, 136: A0+A1 }
     %stack (inB) -> (inB, inB, 148, mul_fp381_12_b01)
     // stack:            inB , inB , 148, ret, out, jumpdest  { 100: H0, 112: H1, 124: H0+H1, 136: A0+A1 }
-    %add_const(12)
+    %shift_bls_fp6
     // stack:            inB1, inB0, 148, ret, out, jumpdest  { 100: H0, 112: H1, 124: H0+H1, 136: A0+A1 }
     %jump(add_fp381_6)
 mul_fp381_12_b01:
     // stack:                                  out, jumpdest  { 100: H0, 112: H1, 124: H0+H1, 136: A0+A1, 148: B0+B1 }
     %stack (out) -> (out, mul_fp381_12_h01, out)
-    %add_const(12)
+    %shift_bls_fp6
     // stack:                       out1, ret, out, jumpdest  { 100: H0, 112: H1, 124: H0+H1, 136: A0+A1, 148: B0+B1 }
     %stack () -> (136, 148)
     // stack:             136, 148, out1, ret, out, jumpdest  { 100: H0, 112: H1, 124: H0+H1, 136: A0+A1, 148: B0+B1 }
@@ -56,13 +56,18 @@ mul_fp381_12_b01:
 mul_fp381_12_h01:
     // stack:                                  out, jumpdest  { 100: H0, 112: H1, 124: H0+H1, out1: H01 }
     %stack (out) -> (out, mul_fp381_12_c1, out)
-    %add_const(12)
+    %shift_bls_fp6
     // stack:                       out1, ret, out, jumpdest  { 100: H0, 112: H1, 124: H0+H1, out1: H01 }
     %stack (out1) -> (out1, 124, out1)
     // stack:            out1, 124, out1, ret, out, jumpdest  { 100: H0, 112: H1, 124: H0+H1, out1: H01 }
     %jump(sub_fp381_6)
 mul_fp381_12_c1:
-    // stack:                                  out, jumpdest  { 100: H0, 112: H1, out1: C1 }
+    // stack:                                  out, jumpdest  { 100: H0, 112:     H1 , out1: C1 }
     %stack () -> (100, 112)
     // stack:                       100, 112, out0, jumpdest  { 100: H0, out0: sh(H1), out1: C1 }
     %jump(add_fp381_6_sh)
+                                                              { out0: C0, out1: C1 }
+
+%macro shift_bls_fp6
+    %add_const(12) 
+%endmacro
