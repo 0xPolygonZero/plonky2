04703717b14bcb4c144f089deafb23cdd4cc34ba
bing <bingcicle@proton.me>
2023-07-11 14:44:51 +0800

    clippy: remove unknown lint

--- plonky2/src/gates/selectors.rs
+++ plonky2/src/gates/selectors.rs
@@ -108,7 +108,6 @@ pub(crate) fn selector_ends_lookups<F: RichField + Extendable<D>, const D: usize
 ///         k
 ///     else
 ///         UNUSED_SELECTOR
-#[allow(clippy::single_range_in_vec_init)]
 pub(crate) fn selector_polynomials<F: RichField + Extendable<D>, const D: usize>(
     gates: &[GateRef<F, D>],
     instances: &[GateInstance<F, D>],
