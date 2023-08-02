e626489e1ce8381eb92a45089e2c3107f5202611
Vivek Pandya <vivekvpandya@gmail.com>
2023-08-02 12:54:32 +0530

    Add serde::Serialize for GenericConfig, implement it on where missing

--- plonky2/src/plonk/config.rs
+++ plonky2/src/plonk/config.rs
@@ -85,7 +85,7 @@ pub trait AlgebraicHasher<F: RichField>: Hasher<F, Hash = HashOut<F>> {
 
 /// Generic configuration trait.
 pub trait GenericConfig<const D: usize>:
-    Debug + Clone + Sync + Sized + Send + Eq + PartialEq
+    Debug + Clone + Sync + Sized + Send + Eq + PartialEq + Serialize
 {
     /// Main field.
     type F: RichField + Extendable<D, Extension = Self::FE>;
@@ -108,7 +108,7 @@ impl GenericConfig<2> for PoseidonGoldilocksConfig {
 }
 
 /// Configuration using truncated Keccak over the Goldilocks field.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Serialize)]
 pub struct KeccakGoldilocksConfig;
 impl GenericConfig<2> for KeccakGoldilocksConfig {
     type F = GoldilocksField;
