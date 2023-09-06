ce478585b39517c82e41537976865da654c5891b
Robin Salen <salenrobin@gmail.com>
2023-09-06 15:22:32 -0400

    Update new test from rebasing

--- evm/tests/log_opcode.rs
+++ evm/tests/log_opcode.rs
@@ -431,7 +431,7 @@ fn test_log_with_aggreg() -> anyhow::Result<()> {
     // Preprocess all circuits.
     let all_circuits = AllRecursiveCircuits::<F, C, D>::new(
         &all_stark,
-        &[16..17, 17..19, 14..15, 9..11, 12..13, 20..21],
+        &[16..17, 17..19, 14..15, 9..11, 12..13, 19..21, 11..13],
         &config,
     );
 
