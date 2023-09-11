2e1d168b09bfffeb67373197afb8cec1eaf5372f
Linda Guiga <lindaguiga3@gmail.com>
2023-09-11 15:48:25 +0100

    Fix Clippy

--- evm/tests/self_balance_gas_cost.rs
+++ evm/tests/self_balance_gas_cost.rs
@@ -104,7 +104,6 @@ fn self_balance_gas_cost() -> anyhow::Result<()> {
         block_gas_used: gas_used.into(),
         block_bloom: [0.into(); 8],
         block_base_fee: 0xa.into(),
-        ..BlockMetadata::default()
     };
 
     let mut contract_code = HashMap::new();
