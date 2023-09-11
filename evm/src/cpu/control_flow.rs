699e5ededa7c1846bbd08bc7387dd1c2d3043353
Linda Guiga <lindaguiga3@gmail.com>
2023-09-11 08:43:42 +0100

    Fix number of native instructions

--- evm/src/cpu/control_flow.rs
+++ evm/src/cpu/control_flow.rs
@@ -8,7 +8,7 @@ use crate::constraint_consumer::{ConstraintConsumer, RecursiveConstraintConsumer
 use crate::cpu::columns::{CpuColumnsView, COL_MAP};
 use crate::cpu::kernel::aggregator::KERNEL;
 
-const NATIVE_INSTRUCTIONS: [usize; 28] = [
+const NATIVE_INSTRUCTIONS: [usize; 27] = [
     COL_MAP.op.add,
     COL_MAP.op.mul,
     COL_MAP.op.sub,
