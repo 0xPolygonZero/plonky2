9508b4909054eebd70c9819810e4885c66e43f96
Robin Salen <30937548+Nashtare@users.noreply.github.com>
2023-09-12 14:45:37 -0400

    Move byte packing / unpacking to a distinct table (#1212)
    * Duplicate Memory trace into BytePacking one
    
    * Add mload_32bytes instruction
    
    * Use dedicated ops for byte packing trace
    
    * Change witness generation to reduce memory reads for MLOAD_32BYTES
    
    * Remove segments
    
    * Fix stack
    
    * Fix extra product when fixing CTL for byte_packing
    
    * Write output value in trace
    
    * Add constraints for BYTE_PACKING table
    
    * Add recursive constraints for BYTE_PACKING table
    
    * Fix constraints
    
    * Add address in trace and constraints
    
    * Add timestamp and batch inputs into BytePackingOp struct
    
    * Add extra column
    
    * Fix BytePackingStark CTL
    
    * Tiny fix in witness generation
    
    * Fix the Memory CTL
    
    * Add constraints for the new columns
    
    * Remove 1 column
    
    * Remove limb columns
    
    * Fix
    
    * Fix recursive circuit of BytePackingTable
    
    * Fix constraints
    
    * Fix endianness
    
    * Add MSTORE_32BYTES instruction and move decomposition to packing table
    
    * Add missing constraint
    
    * Add range-check for all bytes
    
    * Add extra constraint
    
    * Cleanup
    
    * Remove REMAINING_LEN column
    
    * Add corresponding implementations in interpreter
    
    * Fix recursive version
    
    * Remove debug assertion because of CI
    
    * Remove FILTER column
    
    * Update new test from rebasing
    
    * Reorder STARK modules to match TraceCheckPoint ordering
    
    * Address comments
    
    * Pacify clippy
    
    * Add documentation to the packing module
    
    * Fix doctest

--- evm/src/prover.rs
+++ evm/src/prover.rs
@@ -21,6 +21,7 @@ use plonky2_util::{log2_ceil, log2_strict};
 
 use crate::all_stark::{AllStark, Table, NUM_TABLES};
 use crate::arithmetic::arithmetic_stark::ArithmeticStark;
+use crate::byte_packing::byte_packing_stark::BytePackingStark;
 use crate::config::StarkConfig;
 use crate::constraint_consumer::ConstraintConsumer;
 use crate::cpu::cpu_stark::CpuStark;
@@ -53,6 +54,7 @@ where
     F: RichField + Extendable<D>,
     C: GenericConfig<D, F = F>,
     [(); ArithmeticStark::<F, D>::COLUMNS]:,
+    [(); BytePackingStark::<F, D>::COLUMNS]:,
     [(); CpuStark::<F, D>::COLUMNS]:,
     [(); KeccakStark::<F, D>::COLUMNS]:,
     [(); KeccakSpongeStark::<F, D>::COLUMNS]:,
@@ -75,6 +77,7 @@ where
     F: RichField + Extendable<D>,
     C: GenericConfig<D, F = F>,
     [(); ArithmeticStark::<F, D>::COLUMNS]:,
+    [(); BytePackingStark::<F, D>::COLUMNS]:,
     [(); CpuStark::<F, D>::COLUMNS]:,
     [(); KeccakStark::<F, D>::COLUMNS]:,
     [(); KeccakSpongeStark::<F, D>::COLUMNS]:,
@@ -103,6 +106,7 @@ where
     F: RichField + Extendable<D>,
     C: GenericConfig<D, F = F>,
     [(); ArithmeticStark::<F, D>::COLUMNS]:,
+    [(); BytePackingStark::<F, D>::COLUMNS]:,
     [(); CpuStark::<F, D>::COLUMNS]:,
     [(); KeccakStark::<F, D>::COLUMNS]:,
     [(); KeccakSpongeStark::<F, D>::COLUMNS]:,
@@ -193,6 +197,7 @@ where
     F: RichField + Extendable<D>,
     C: GenericConfig<D, F = F>,
     [(); ArithmeticStark::<F, D>::COLUMNS]:,
+    [(); BytePackingStark::<F, D>::COLUMNS]:,
     [(); CpuStark::<F, D>::COLUMNS]:,
     [(); KeccakStark::<F, D>::COLUMNS]:,
     [(); KeccakSpongeStark::<F, D>::COLUMNS]:,
@@ -208,6 +213,19 @@ where
             &trace_poly_values[Table::Arithmetic as usize],
             &trace_commitments[Table::Arithmetic as usize],
             &ctl_data_per_table[Table::Arithmetic as usize],
+            challenger,
+            timing,
+        )?
+    );
+    let byte_packing_proof = timed!(
+        timing,
+        "prove byte packing STARK",
+        prove_single_table(
+            &all_stark.byte_packing_stark,
+            config,
+            &trace_poly_values[Table::BytePacking as usize],
+            &trace_commitments[Table::BytePacking as usize],
+            &ctl_data_per_table[Table::BytePacking as usize],
             challenger,
             timing,
         )?
@@ -277,8 +295,10 @@ where
             timing,
         )?
     );
+
     Ok([
         arithmetic_proof,
+        byte_packing_proof,
         cpu_proof,
         keccak_proof,
         keccak_sponge_proof,
