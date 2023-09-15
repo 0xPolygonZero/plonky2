c7a83f54d190194b37666f6d9956415fc8535302
Robin Salen <salenrobin@gmail.com>
2023-09-15 16:17:41 -0400

    Move halting logic constraints to dedicated module

--- evm/src/cpu/control_flow.rs
+++ evm/src/cpu/control_flow.rs
@@ -4,7 +4,6 @@ use plonky2::field::types::Field;
 use plonky2::hash::hash_types::RichField;
 use plonky2::iop::ext_target::ExtensionTarget;
 
-use super::membus::NUM_GP_CHANNELS;
 use crate::constraint_consumer::{ConstraintConsumer, RecursiveConstraintConsumer};
 use crate::cpu::columns::{CpuColumnsView, COL_MAP};
 use crate::cpu::kernel::aggregator::KERNEL;
@@ -54,13 +53,7 @@ pub fn eval_packed_generic<P: PackedField>(
     let is_cpu_cycle: P = COL_MAP.op.iter().map(|&col_i| lv[col_i]).sum();
     let is_cpu_cycle_next: P = COL_MAP.op.iter().map(|&col_i| nv[col_i]).sum();
 
-    let halt_state = P::ONES - lv.is_bootstrap_kernel - is_cpu_cycle;
     let next_halt_state = P::ONES - nv.is_bootstrap_kernel - is_cpu_cycle_next;
-
-    // The null special instruction must be boolean.
-    yield_constr.constraint(halt_state * (halt_state - P::ONES));
-    // Once we reach a null row, there must be only null rows.
-    yield_constr.constraint_transition(halt_state * (next_halt_state - P::ONES));
 
     // Once we start executing instructions, then we continue until the end of the table
     // or we reach dummy padding rows. This, along with the constraints on the first row,
@@ -88,23 +81,6 @@ pub fn eval_packed_generic<P: PackedField>(
     yield_constr.constraint_transition(is_last_noncpu_cycle * pc_diff);
     yield_constr.constraint_transition(is_last_noncpu_cycle * (nv.is_kernel_mode - P::ONES));
     yield_constr.constraint_transition(is_last_noncpu_cycle * nv.stack_len);
-
-    // Padding rows should have their memory channels disabled.
-    for i in 0..NUM_GP_CHANNELS {
-        let channel = lv.mem_channels[i];
-        yield_constr.constraint(halt_state * channel.used);
-    }
-
-    // The last row must be a dummy padding row.
-    yield_constr.constraint_last_row(halt_state - P::ONES);
-
-    // Also, a padding row's `program_counter` must be at the `halt` label.
-    // In particular, it ensures that the first padding row may only be added
-    // after we jumped to the `halt` function. Subsequent padding rows may set
-    // the `program_counter` to arbitrary values (there's no transition
-    // constraints) so we can place this requirement on them too.
-    let halt_pc = get_halt_pc::<P::Scalar>();
-    yield_constr.constraint(halt_state * (lv.program_counter - halt_pc));
 }
 
 pub fn eval_ext_circuit<F: RichField + Extendable<D>, const D: usize>(
@@ -118,17 +94,8 @@ pub fn eval_ext_circuit<F: RichField + Extendable<D>, const D: usize>(
     let is_cpu_cycle = builder.add_many_extension(COL_MAP.op.iter().map(|&col_i| lv[col_i]));
     let is_cpu_cycle_next = builder.add_many_extension(COL_MAP.op.iter().map(|&col_i| nv[col_i]));
 
-    let halt_state = builder.add_extension(lv.is_bootstrap_kernel, is_cpu_cycle);
-    let halt_state = builder.sub_extension(one, halt_state);
     let next_halt_state = builder.add_extension(nv.is_bootstrap_kernel, is_cpu_cycle_next);
     let next_halt_state = builder.sub_extension(one, next_halt_state);
-
-    // The null special instruction must be boolean.
-    let constr = builder.mul_sub_extension(halt_state, halt_state, halt_state);
-    yield_constr.constraint(builder, constr);
-    // Once we reach a null row, there must be only null rows.
-    let constr = builder.mul_sub_extension(halt_state, next_halt_state, halt_state);
-    yield_constr.constraint_transition(builder, constr);
 
     // Once we start executing instructions, then we continue until the end of the table
     // or we reach dummy padding rows. This, along with the constraints on the first row,
@@ -178,33 +145,5 @@ pub fn eval_ext_circuit<F: RichField + Extendable<D>, const D: usize>(
         // Start with empty stack
         let kernel_constr = builder.mul_extension(is_last_noncpu_cycle, nv.stack_len);
         yield_constr.constraint_transition(builder, kernel_constr);
-    }
-
-    // Padding rows should have their memory channels disabled.
-    for i in 0..NUM_GP_CHANNELS {
-        let channel = lv.mem_channels[i];
-        let constr = builder.mul_extension(halt_state, channel.used);
-        yield_constr.constraint(builder, constr);
-    }
-
-    // The last row must be a dummy padding row.
-    {
-        let one = builder.one_extension();
-        let constr = builder.sub_extension(halt_state, one);
-        yield_constr.constraint_last_row(builder, constr);
-    }
-
-    // Also, a padding row's `program_counter` must be at the `halt` label.
-    // In particular, it ensures that the first padding row may only be added
-    // after we jumped to the `halt` function. Subsequent padding rows may set
-    // the `program_counter` to arbitrary values (there's no transition
-    // constraints) so we can place this requirement on them too.
-    {
-        let halt_pc = get_halt_pc();
-        let halt_pc_target = builder.constant_extension(halt_pc);
-        let constr = builder.sub_extension(lv.program_counter, halt_pc_target);
-        let constr = builder.mul_extension(halt_state, constr);
-
-        yield_constr.constraint(builder, constr);
     }
 }
