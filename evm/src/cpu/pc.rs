1d60431992ab3cc90addfa12b43f851e35ad97cb
Hamy Ratoanina <hamy.ratoanina@toposware.com>
2023-10-11 22:28:49 +0200

    Store top of the stack in memory channel 0 (#1215)
    * Store top of the stack in memory channel 0
    
    * Fix interpreter
    
    * Apply comments
    
    * Remove debugging code
    
    * Merge commit
    
    * Remove debugging comments
    
    * Apply comments
    
    * Fix witness generation for exceptions
    
    * Fix witness generation for exceptions (again)
    
    * Fix modfp254 constraint

--- evm/src/cpu/pc.rs
+++ evm/src/cpu/pc.rs
@@ -5,16 +5,16 @@ use plonky2::iop::ext_target::ExtensionTarget;
 
 use crate::constraint_consumer::{ConstraintConsumer, RecursiveConstraintConsumer};
 use crate::cpu::columns::CpuColumnsView;
-use crate::cpu::membus::NUM_GP_CHANNELS;
 
 pub fn eval_packed<P: PackedField>(
     lv: &CpuColumnsView<P>,
+    nv: &CpuColumnsView<P>,
     yield_constr: &mut ConstraintConsumer<P>,
 ) {
     let filter = lv.op.pc;
-    let push_value = lv.mem_channels[NUM_GP_CHANNELS - 1].value;
-    yield_constr.constraint(filter * (push_value[0] - lv.program_counter));
-    for &limb in &push_value[1..] {
+    let new_stack_top = nv.mem_channels[0].value;
+    yield_constr.constraint(filter * (new_stack_top[0] - lv.program_counter));
+    for &limb in &new_stack_top[1..] {
         yield_constr.constraint(filter * limb);
     }
 }
@@ -22,16 +22,17 @@ pub fn eval_packed<P: PackedField>(
 pub fn eval_ext_circuit<F: RichField + Extendable<D>, const D: usize>(
     builder: &mut plonky2::plonk::circuit_builder::CircuitBuilder<F, D>,
     lv: &CpuColumnsView<ExtensionTarget<D>>,
+    nv: &CpuColumnsView<ExtensionTarget<D>>,
     yield_constr: &mut RecursiveConstraintConsumer<F, D>,
 ) {
     let filter = lv.op.pc;
-    let push_value = lv.mem_channels[NUM_GP_CHANNELS - 1].value;
+    let new_stack_top = nv.mem_channels[0].value;
     {
-        let diff = builder.sub_extension(push_value[0], lv.program_counter);
+        let diff = builder.sub_extension(new_stack_top[0], lv.program_counter);
         let constr = builder.mul_extension(filter, diff);
         yield_constr.constraint(builder, constr);
     }
-    for &limb in &push_value[1..] {
+    for &limb in &new_stack_top[1..] {
         let constr = builder.mul_extension(filter, limb);
         yield_constr.constraint(builder, constr);
     }
