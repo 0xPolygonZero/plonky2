cc204aecd95834bee83426cc2ee47945d11835bd
4l0n50 <alonso.gon@gmail.com>
2024-01-11 18:14:50 +0100

    Remove debug info and fix clippy

--- evm/src/generation/rlp.rs
+++ evm/src/generation/rlp.rs
@@ -6,45 +6,17 @@ pub(crate) fn all_rlp_prover_inputs_reversed(signed_txn: &[u8]) -> Vec<U256> {
     inputs
 }
 
-pub(crate) fn all_rlp_prover_inputs_reversed_old(signed_txn: &[u8]) -> Vec<U256> {
-    let mut inputs = all_rlp_prover_inputs_old(signed_txn);
-    inputs.reverse();
-    inputs
-}
-
 fn all_rlp_prover_inputs(signed_txn: &[u8]) -> Vec<U256> {
     let mut prover_inputs = vec![];
     prover_inputs.push(signed_txn.len().into());
     let mut chunks = signed_txn.chunks_exact(32);
-    while let Some(bytes) = chunks.next() {
+    for bytes in chunks.by_ref() {
         prover_inputs.push(U256::from_big_endian(bytes));
     }
     let mut last_chunk = chunks.remainder().to_vec();
-    if last_chunk.len() > 0 {
+    if !last_chunk.is_empty() {
         last_chunk.extend_from_slice(&vec![0u8; 32 - last_chunk.len()]);
         prover_inputs.push(U256::from_big_endian(&last_chunk));
     }
-    log::debug!(
-        "rlp_prover_inputs = {:?}",
-        prover_inputs
-            .iter()
-            .map(|x| {
-                let mut bytes = [0u8; 32];
-                x.to_big_endian(&mut bytes);
-                bytes.to_vec()
-            })
-            .flatten()
-            .collect::<Vec<u8>>()
-    );
-    prover_inputs
-}
-
-fn all_rlp_prover_inputs_old(signed_txn: &[u8]) -> Vec<U256> {
-    let mut prover_inputs = vec![];
-    prover_inputs.push(signed_txn.len().into());
-    for &byte in signed_txn {
-        prover_inputs.push(byte.into());
-    }
-    log::debug!("rlp_prover_inputs_old = {:?}", prover_inputs);
     prover_inputs
 }
--- evm/src/generation/state.rs
+++ evm/src/generation/state.rs
@@ -10,7 +10,7 @@ use super::mpt::{load_all_mpts, TrieRootPtrs};
 use super::TrieInputs;
 use crate::cpu::kernel::aggregator::KERNEL;
 use crate::cpu::kernel::constants::context_metadata::ContextMetadata;
-use crate::generation::rlp::{all_rlp_prover_inputs_reversed, all_rlp_prover_inputs_reversed_old};
+use crate::generation::rlp::all_rlp_prover_inputs_reversed;
 use crate::generation::GenerationInputs;
 use crate::memory::segments::Segment;
 use crate::util::u256_to_usize;
@@ -35,8 +35,6 @@ pub(crate) struct GenerationState<F: Field> {
     /// Prover inputs containing RLP data, in reverse order so that the next input can be obtained
     /// via `pop()`.
     pub(crate) rlp_prover_inputs: Vec<U256>,
-
-    pub(crate) rlp_prover_inputs_old: Vec<U256>,
 
     pub(crate) withdrawal_prover_inputs: Vec<U256>,
 
@@ -82,9 +80,6 @@ impl<F: Field> GenerationState<F> {
 
         let rlp_prover_inputs =
             all_rlp_prover_inputs_reversed(inputs.clone().signed_txn.as_ref().unwrap_or(&vec![]));
-        let rlp_prover_inputs_old = all_rlp_prover_inputs_reversed_old(
-            inputs.clone().signed_txn.as_ref().unwrap_or(&vec![]),
-        );
         let withdrawal_prover_inputs = all_withdrawals_prover_inputs_reversed(&inputs.withdrawals);
         let bignum_modmul_result_limbs = Vec::new();
 
@@ -94,7 +89,6 @@ impl<F: Field> GenerationState<F> {
             memory: MemoryState::new(kernel_code),
             traces: Traces::default(),
             rlp_prover_inputs,
-            rlp_prover_inputs_old,
             withdrawal_prover_inputs,
             state_key_to_address: HashMap::new(),
             bignum_modmul_result_limbs,
