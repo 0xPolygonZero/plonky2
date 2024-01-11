7aece64678c167f675f6746bc368a9739cec925c
4l0n50 <alonso.gon@gmail.com>
2024-01-11 18:02:30 +0100

    Fix endianness bug

--- evm/src/cpu/kernel/asm/rlp/read_to_memory.asm
+++ evm/src/cpu/kernel/asm/rlp/read_to_memory.asm
@@ -7,36 +7,23 @@
 global read_rlp_to_memory:
     // stack: retdest
     PROVER_INPUT(rlp) // Read the RLP blob length from the prover tape.
-    PROVER_INPUT(rlp_old)
-global debug_mira_lo_que_puede_hacer_la_maldad:
-    POP // debug only! elimnate
     // stack: len, retdest
     PUSH @SEGMENT_RLP_RAW
     %build_kernel_address
 
     PUSH @SEGMENT_RLP_RAW // ctx == virt == 0
     // stack: addr, final_addr, retdest
-
-global debug_el_misterio_del_amor:
 read_rlp_to_memory_loop:
     // stack: addr, final_addr, retdest
     DUP2
     DUP2
-global debug_before_lt:
     LT
-global debug_before_not:
     ISZERO
-global debug_no_puede_sel_seniol_gesu:
     // stack: addr >= final_addr, addr, final_addr, retdest
     %jumpi(read_rlp_to_memory_finish)
     // stack: addr, final_addr, retdest
     PROVER_INPUT(rlp)
-    PROVER_INPUT(rlp_old) // debug_elimnate
-global debug_rlp_old:
-    POP //debug_elminate
     SWAP1
-    // stack: addr, packed_bytes, final_addr, retdest
-global debug_chapalapachala:
     MSTORE_32BYTES_32
     // stack: addr', final_addr, retdest
     %jump(read_rlp_to_memory_loop)
@@ -48,5 +35,4 @@ read_rlp_to_memory_finish:
     DUP3 SUB
     // stack: pos, addr, final_addr, retdest
     %stack(pos, addr, final_addr, retdest) -> (retdest, pos)
-global debug_before_jumping_to_another_dimesion_where_dogs_are_called_cats_and_cats_are_called_cats:
     JUMP
--- evm/src/generation/rlp.rs
+++ evm/src/generation/rlp.rs
@@ -15,10 +15,27 @@ pub(crate) fn all_rlp_prover_inputs_reversed_old(signed_txn: &[u8]) -> Vec<U256>
 fn all_rlp_prover_inputs(signed_txn: &[u8]) -> Vec<U256> {
     let mut prover_inputs = vec![];
     prover_inputs.push(signed_txn.len().into());
-    for bytes in signed_txn.chunks(32) {
+    let mut chunks = signed_txn.chunks_exact(32);
+    while let Some(bytes) = chunks.next() {
         prover_inputs.push(U256::from_big_endian(bytes));
     }
-    log::debug!("rlp_prover_inputs = {:?}", prover_inputs);
+    let mut last_chunk = chunks.remainder().to_vec();
+    if last_chunk.len() > 0 {
+        last_chunk.extend_from_slice(&vec![0u8; 32 - last_chunk.len()]);
+        prover_inputs.push(U256::from_big_endian(&last_chunk));
+    }
+    log::debug!(
+        "rlp_prover_inputs = {:?}",
+        prover_inputs
+            .iter()
+            .map(|x| {
+                let mut bytes = [0u8; 32];
+                x.to_big_endian(&mut bytes);
+                bytes.to_vec()
+            })
+            .flatten()
+            .collect::<Vec<u8>>()
+    );
     prover_inputs
 }
 
