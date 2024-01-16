af51822a45659a5e337e93cec0890f7f17179334
Robin Salen <salenrobin@gmail.com>
2024-01-16 17:29:16 -0500

    Skip rep loop

--- evm/src/cpu/kernel/asm/core/create_receipt.asm
+++ evm/src/cpu/kernel/asm/core/create_receipt.asm
@@ -206,19 +206,10 @@ process_receipt_after_write:
     DUP5
     %mpt_insert_receipt_trie
     // stack: new_cum_gas, txn_nb, num_nibbles, retdest
-    // Now, we set the Bloom filter back to 0. We proceed by chunks of 32 bytes.
-    PUSH @SEGMENT_TXN_BLOOM // ctx == offset == 0
-    %rep 8
-        // stack: addr, new_cum_gas, txn_nb, num_nibbles, retdest
-        PUSH 0 // we will fill the memory segment with zeroes
-        DUP2
-        // stack: addr, 0, addr, new_cum_gas, txn_nb, num_nibbles, retdest
-        MSTORE_32BYTES_32
-        // stack: new_addr, addr, new_cum_gas, txn_nb, num_nibbles, retdest
-        SWAP1 POP
-    %endrep
-    POP
-    // stack: new_cum_gas, txn_nb, num_nibbles, retdest
+
+    // We don't need to reset the bloom filter segment as we only process a single transaction.
+    // TODO: Revert in case we add back support for multi-txn proofs.
+
     %stack (new_cum_gas, txn_nb, num_nibbles, retdest) -> (retdest, new_cum_gas)
     JUMP
     
