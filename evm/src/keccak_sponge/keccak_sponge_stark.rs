635796365406419372717259d70c70d6dc6fdaae
Robin Salen <30937548+Nashtare@users.noreply.github.com>
2024-02-01 18:19:49 -0500

    Reorganize lookup / ctl modules (#1495)
    * Reorganize lookup / ctl modules
    
    * Apply review

--- evm/src/keccak_sponge/keccak_sponge_stark.rs
+++ evm/src/keccak_sponge/keccak_sponge_stark.rs
@@ -17,10 +17,9 @@ use plonky2_util::ceil_div_usize;
 
 use crate::constraint_consumer::{ConstraintConsumer, RecursiveConstraintConsumer};
 use crate::cpu::kernel::keccak_util::keccakf_u32s;
-use crate::cross_table_lookup::{Column, Filter};
 use crate::evaluation_frame::{StarkEvaluationFrame, StarkFrame};
 use crate::keccak_sponge::columns::*;
-use crate::lookup::Lookup;
+use crate::lookup::{Column, Filter, Lookup};
 use crate::stark::Stark;
 use crate::witness::memory::MemoryAddress;
 
