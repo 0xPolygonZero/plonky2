1a00f3c8dec4e43fb01114d8f0425a3074ade737
Alonso González <alonso.gon@gmail.com>
2024-02-08 13:38:05 +0100

    Apply suggestions from code review
    Co-authored-by: Robin Salen <30937548+Nashtare@users.noreply.github.com>

--- evm/src/generation/prover_input.rs
+++ evm/src/generation/prover_input.rs
@@ -250,7 +250,7 @@ impl<F: Field> GenerationState<F> {
         }
     }
 
-    /// Generate either the next used jump address or the proof for the last jump address.
+    /// Generates either the next used jump address or the proof for the last jump address.
     fn run_access_lists(&mut self, input_fn: &ProverInputFn) -> Result<U256, ProgramError> {
         match input_fn.0[1].as_str() {
             "address_insert" => self.run_next_addresses_insert(),
@@ -305,8 +305,8 @@ impl<F: Field> GenerationState<F> {
         }
     }
 
-    /// Returns the a pointer to an element in the list whose value val is such that
-    /// val <= addr < next_val and addr is the top of the stack.
+    /// Returns a pointer to an element in the list whose value is such that
+    /// `value <= addr < next_value` and `addr` is the top of the stack.
     fn run_next_addresses_insert(&mut self) -> Result<U256, ProgramError> {
         let addr = stack_peek(self, 0)?;
         for (curr_ptr, next_addr, _) in self.get_addresses_access_list()? {
@@ -317,9 +317,9 @@ impl<F: Field> GenerationState<F> {
         Ok((Segment::AccessedAddresses as usize).into())
     }
 
-    /// Returns the a pointer to an element in the list whose value val is such that
-    /// val < addr == next_val and addr is the top of the stack. If the element is not
-    /// in the list returns loops forever
+    /// Returns a pointer to an element in the list whose value is such that
+    /// `value < addr == next_value` and addr is the top of the stack.
+    /// If the element is not in the list returns loops forever
     fn run_next_addresses_remove(&mut self) -> Result<U256, ProgramError> {
         let addr = stack_peek(self, 0)?;
         for (curr_ptr, next_addr, _) in self.get_addresses_access_list()? {
@@ -330,7 +330,7 @@ impl<F: Field> GenerationState<F> {
         Ok((Segment::AccessedAddresses as usize).into())
     }
 
-    /// Returns the a pointer to the predecessor of the top of the stack in the accessed storage keys list.
+    /// Returns a pointer to the predecessor of the top of the stack in the accessed storage keys list.
     fn run_next_storage_insert(&mut self) -> Result<U256, ProgramError> {
         let addr = stack_peek(self, 0)?;
         let key = stack_peek(self, 1)?;
@@ -342,7 +342,7 @@ impl<F: Field> GenerationState<F> {
         Ok((Segment::AccessedAddresses as usize).into())
     }
 
-    /// Returns the a pointer to the predecessor of the top of the stack in the accessed storage keys list.
+    /// Returns a pointer to the predecessor of the top of the stack in the accessed storage keys list.
     fn run_next_storage_remove(&mut self) -> Result<U256, ProgramError> {
         let addr = stack_peek(self, 0)?;
         let key = stack_peek(self, 1)?;
@@ -611,9 +611,9 @@ impl Iterator for AccList {
         if let Ok(new_pos) = u256_to_usize(self.access_list_mem[self.pos + self.node_size - 1]) {
             let old_pos = self.pos;
             self.pos = new_pos - self.offset;
-            if self.node_size < 3 {
+            if self.node_size == 2 { // addresses
                 Some((old_pos, self.access_list_mem[self.pos], U256::zero()))
-            } else {
+            } else { // storage_keys
                 Some((
                     old_pos,
                     self.access_list_mem[self.pos],
