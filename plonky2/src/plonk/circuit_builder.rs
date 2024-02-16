3a30467894cbb0be8bf3633b7a5544bdfbc4282a
Hamy Ratoanina <hamy.ratoanina@toposware.com>
2024-02-16 11:09:44 -0500

    Add hash public input methods

--- plonky2/src/plonk/circuit_builder.rs
+++ plonky2/src/plonk/circuit_builder.rs
@@ -343,6 +343,13 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
         HashOutTarget::from_vec(self.add_virtual_targets(4))
     }
 
+    /// Registers a new `HashOutTarget` as a public input. `NUM_HASH_OUT_ELTS`
+    /// being hardcoded to 4, it internally adds 4 virtual targets in a
+    /// vector fashion.
+    pub fn add_virtual_hash_public_input(&mut self) -> HashOutTarget {
+        HashOutTarget::from_vec(self.add_virtual_public_input_arr::<4>().into())
+    }
+
     /// Adds a new `MerkleCapTarget`, consisting in `1 << cap_height` `HashOutTarget`.
     pub fn add_virtual_cap(&mut self, cap_height: usize) -> MerkleCapTarget {
         MerkleCapTarget(self.add_virtual_hashes(1 << cap_height))
@@ -351,6 +358,13 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
     /// Adds `n` new `HashOutTarget` in a vector fashion.
     pub fn add_virtual_hashes(&mut self, n: usize) -> Vec<HashOutTarget> {
         (0..n).map(|_i| self.add_virtual_hash()).collect()
+    }
+
+    /// Registers `n` new `HashOutTarget` as public inputs, in a vector fashion.
+    pub fn add_virtual_hashes_public_input(&mut self, n: usize) -> Vec<HashOutTarget> {
+        (0..n)
+            .map(|_i| self.add_virtual_hash_public_input())
+            .collect()
     }
 
     pub(crate) fn add_virtual_merkle_proof(&mut self, len: usize) -> MerkleProofTarget {
