0817fd8e983d82aa0332726cf633584da495cb24
Robin Salen <30937548+Nashtare@users.noreply.github.com>
2024-02-28 15:21:34 +0900

    Pacify clippy (#1547)

--- field/src/extension/mod.rs
+++ field/src/extension/mod.rs
@@ -125,9 +125,9 @@ impl<F: Field> FieldExtension<1> for F {
 }
 
 /// Flatten the slice by sending every extension field element to its D-sized canonical representation.
-pub fn flatten<F: Field, const D: usize>(l: &[F::Extension]) -> Vec<F>
+pub fn flatten<F, const D: usize>(l: &[F::Extension]) -> Vec<F>
 where
-    F: Extendable<D>,
+    F: Field + Extendable<D>,
 {
     l.iter()
         .flat_map(|x| x.to_basefield_array().to_vec())
@@ -135,9 +135,9 @@ where
 }
 
 /// Batch every D-sized chunks into extension field elements.
-pub fn unflatten<F: Field, const D: usize>(l: &[F]) -> Vec<F::Extension>
+pub fn unflatten<F, const D: usize>(l: &[F]) -> Vec<F::Extension>
 where
-    F: Extendable<D>,
+    F: Field + Extendable<D>,
 {
     debug_assert_eq!(l.len() % D, 0);
     l.chunks_exact(D)
