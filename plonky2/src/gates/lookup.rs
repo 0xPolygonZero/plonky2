4857d16fc53170778dacb8b4c6d1947c698c48aa
Daniel Bloom <7810950-Daniel.Aaron.Bloom@users.noreply.gitlab.com>
2024-04-16 00:32:04 -0700

    refactor: switch to `tiny-keccak` library

--- plonky2/src/gates/lookup.rs
+++ plonky2/src/gates/lookup.rs
@@ -7,7 +7,6 @@ use alloc::{
 };
 
 use itertools::Itertools;
-use keccak_hash::keccak;
 
 use super::lookup_table::LookupTable;
 use crate::field::extension::Extendable;
@@ -16,6 +15,7 @@ use crate::gates::gate::Gate;
 use crate::gates::packed_util::PackedEvaluableBase;
 use crate::gates::util::StridedConstraintConsumer;
 use crate::hash::hash_types::RichField;
+use crate::hash::keccak::keccak;
 use crate::iop::ext_target::ExtensionTarget;
 use crate::iop::generator::{GeneratedValues, SimpleGenerator, WitnessGeneratorRef};
 use crate::iop::target::Target;
--- plonky2/src/gates/lookup_table.rs
+++ plonky2/src/gates/lookup_table.rs
@@ -10,7 +10,6 @@ use alloc::{
 use std::sync::Arc;
 
 use itertools::Itertools;
-use keccak_hash::keccak;
 use plonky2_util::ceil_div_usize;
 
 use crate::field::extension::Extendable;
@@ -19,6 +18,7 @@ use crate::gates::gate::Gate;
 use crate::gates::packed_util::PackedEvaluableBase;
 use crate::gates::util::StridedConstraintConsumer;
 use crate::hash::hash_types::RichField;
+use crate::hash::keccak::keccak;
 use crate::iop::ext_target::ExtensionTarget;
 use crate::iop::generator::{GeneratedValues, SimpleGenerator, WitnessGeneratorRef};
 use crate::iop::target::Target;
