9fabe0e08025445f2b2c8a0e7a7e342ee9511c0a
Sai Deng <deng.sai@gmail.com>
2024-04-18 09:53:01 -0700

    assert

--- plonky2/src/fri/batch_verifier.rs
+++ plonky2/src/fri/batch_verifier.rs
@@ -236,6 +236,7 @@ fn batch_fri_verifier_query_round<
             batch_index += 1;
         }
     }
+    assert_eq!(batch_index, instance.len(), "Wrong number of folded instances.");
 
     // Final check of FRI. After all the reductions, we check that the final polynomial is equal
     // to the one sent by the prover.
