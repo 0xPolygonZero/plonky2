90f96b88eeac3167dc52d58d42227ccfc86dd7e0
Hamy Ratoanina <hamy.ratoanina@toposware.com>
2024-04-25 17:58:16 -0400

    Change visibility of dummy_circuit

--- plonky2/src/recursion/dummy_circuit.rs
+++ plonky2/src/recursion/dummy_circuit.rs
@@ -67,11 +67,7 @@ where
 /// Generate a proof for a dummy circuit. The `public_inputs` parameter let the caller specify
 /// certain public inputs (identified by their indices) which should be given specific values.
 /// The rest will default to zero.
-pub(crate) fn dummy_proof<
-    F: RichField + Extendable<D>,
-    C: GenericConfig<D, F = F>,
-    const D: usize,
->(
+pub fn dummy_proof<F: RichField + Extendable<D>, C: GenericConfig<D, F = F>, const D: usize>(
     circuit: &CircuitData<F, C, D>,
     nonzero_public_inputs: HashMap<usize, F>,
 ) -> anyhow::Result<ProofWithPublicInputs<F, C, D>>
@@ -86,11 +82,7 @@ where
 }
 
 /// Generate a circuit matching a given `CommonCircuitData`.
-pub(crate) fn dummy_circuit<
-    F: RichField + Extendable<D>,
-    C: GenericConfig<D, F = F>,
-    const D: usize,
->(
+pub fn dummy_circuit<F: RichField + Extendable<D>, C: GenericConfig<D, F = F>, const D: usize>(
     common_data: &CommonCircuitData<F, D>,
 ) -> CircuitData<F, C, D> {
     let config = common_data.config.clone();
