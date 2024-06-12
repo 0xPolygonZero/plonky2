f6a08bdb9476102144a2afc994ab724cea93e145
Sai Deng <deng.sai@gmail.com>
2024-06-12 08:24:11 +0800

    add batch starks

--- plonky2/src/fri/mod.rs
+++ plonky2/src/fri/mod.rs
@@ -10,6 +10,10 @@ use serde::Serialize;
 
 use crate::fri::reduction_strategies::FriReductionStrategy;
 
+pub mod batch_oracle;
+pub mod batch_prover;
+pub mod batch_recursive_verifier;
+pub mod batch_verifier;
 mod challenges;
 pub mod oracle;
 pub mod proof;
