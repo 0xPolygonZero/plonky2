b38d0753c1b271ca582d53382a42f458e5e09d7e
Robin Salen <salenrobin@gmail.com>
2024-08-27 16:51:58 -0400

    Wording

--- plonky2/src/plonk/circuit_builder.rs
+++ plonky2/src/plonk/circuit_builder.rs
@@ -524,7 +524,7 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
             .push(CopyConstraint::new((x, y), self.context_log.open_stack()));
     }
 
-    /// Enforces that two [`Target`] arrays are equal.
+    /// Enforces that the underlying values of two [`Target`] arrays are equal.
     pub fn connect_array<const N: usize>(&mut self, x: [Target; N], y: [Target; N]) {
         for i in 0..N {
             self.connect(x[i], y[i]);
